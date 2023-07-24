55203770c46f99d9e9691ace580ea96d900de640
cravela <cravela@intelops.dev>
2023-07-24 16:28:58 -0500

    feat: added inline comments.
    Signed-off-by: cravela <cravela@intelops.dev>

--- pkg/ebpf/c/process_exit/process_exit.go
+++ pkg/ebpf/c/process_exit/process_exit.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 Authors of Tarian & the Organization created Tarian
+
 package process_exit
 
 import (
@@ -9,7 +12,9 @@ import (
 	"github.com/cilium/ebpf/ringbuf"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags $BPF_CFLAGS -type event_data exit exit.bpf.c -- -I../../../../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags $BPF_CFLAGS -target $CURR_ARCH -type event_data exit exit.bpf.c -- -I../../../../headers
+
+// loads the ebpf specs like maps, programs
 func getEbpfObject() (*exitObjects, error) {
 	var bpfObj exitObjects
 	err := loadExitObjects(&bpfObj, nil)
@@ -29,10 +34,14 @@ type ProcessExitDetector struct {
 	ringbufReader *ringbuf.Reader
 }
 
+// NewProcessExitDetector returns a new instance of ProcessExitDetector
 func NewProcessExitDetector() *ProcessExitDetector {
 	return &ProcessExitDetector{}
 }
 
+// Start the close detector by attaching ebpf program to
+// hook in kernel and opens the map to read the data.
+// If it cannot be started an error is returned.
 func (p *ProcessExitDetector) Start() error {
 	bpfObjs, err := getEbpfObject()
 	if err != nil {
@@ -57,6 +66,7 @@ func (p *ProcessExitDetector) Start() error {
 	return nil
 }
 
+// closes the EBPF objects
 func (p *ProcessExitDetector) Close() error {
 	err := p.ebpfLink.Close()
 	if err != nil {
@@ -66,8 +76,10 @@ func (p *ProcessExitDetector) Close() error {
 	return p.ringbufReader.Close()
 }
 
+// reads the next event from the ringbuffer
 func (p *ProcessExitDetector) Read() (ExitEventData, error) {
 	var event ExitEventData
+	// reads the data from ringbuffer
 	record, err := p.ringbufReader.Read()
 	if err != nil {
 		if errors.Is(err, ringbuf.ErrClosed) {
@@ -76,7 +88,7 @@ func (p *ProcessExitDetector) Read() (ExitEventData, error) {
 		return event, err
 	}
 
-	// Parse the ringbuf event exit into a bpfEvent structure.
+	// read the raw sample from the record.RawSample
 	if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
 		return event, err
 	}
@@ -84,6 +96,7 @@ func (p *ProcessExitDetector) Read() (ExitEventData, error) {
 	return event, nil
 }
 
+// reads data from a ring buffer and returns it as an interface
 func (p *ProcessExitDetector) ReadAsInterface() (any, error) {
 	return p.Read()
 }
