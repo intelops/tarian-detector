19139f81cb1c14d0c95f1eb16476f5374219bbc5
AnamikaSanjay <asanjay@intelops.dev>
2023-07-25 10:58:48 -0500

    added comments and removed debug print function

--- pkg/ebpf/c/network_listen/listen.bpf.c
+++ pkg/ebpf/c/network_listen/listen.bpf.c
@@ -21,18 +21,28 @@ struct
 
 SEC("kprobe/__x64_sys_listen")
 /**
-* This is the implementation of kretprobe to the  function __x64_sys_listen .This function is used to set the listen bit in sys_regs.
-* @param ctx - * Pointer to the structure containing registers set by the user.
-* @return Returns 0 on success non - zero on failure. In the case of failure the error code is returned
-*/
+ * kprobe_listen is a kprobe attached to the function __x64_sys_listen.
+ * It captures the arguments of the function call and emits them as an event.
+ *
+ * @param ctx - Pointer to the structure containing registers set by the kernel.
+ * @return Returns 0 to continue the execution of the probed function.
+ */
 int kprobe_listen(struct pt_regs *ctx)
 {
     struct event_data args = {};
+    
     struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1(ctx);
+   
+    // Read the first argument of __x64_sys_listen and store it in args.
     bpf_probe_read(&args.args[0], sizeof(args.args[0]), &PT_REGS_PARM1(ctx2));
-    bpf_probe_read(&args.args[0], sizeof(args.args[0]), &PT_REGS_PARM1(ctx2));
+
+    // Read the second argument of __x64_sys_listen and store it in args.
     bpf_probe_read(&args.args[1], sizeof(args.args[1]), &PT_REGS_PARM2(ctx2));
+
+    // Emit the captured arguments as an event.
     bpf_perf_event_output(ctx, &event, BPF_F_CURRENT_CPU, &args, sizeof(args));
+    
+    // Return 0 to continue the execution of the probed function.
     return 0;
 }
 
