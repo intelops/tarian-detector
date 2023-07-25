13df3269a3784f9c643fb1712efec2252e8bcd65
AnamikaSanjay <asanjay@intelops.dev>
2023-07-25 11:41:13 -0500

    added comments in kprobe bind

--- pkg/ebpf/c/network_bind/bind.bpf.c
+++ pkg/ebpf/c/network_bind/bind.bpf.c
@@ -7,18 +7,21 @@
 #include "bpf_helpers.h"
 #include "bpf_core_read.h"
 
+// Define the structure for event data
 struct event_data
 {
     unsigned long args[3];
 };
 const struct event_data *unused __attribute__((unused));
 
+// Define the perf event array map with maximum 1 << 24 entries
 struct
 {
     __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
     __uint(max_entries, 1 << 24);
 } event SEC(".maps");
 
+// Define the kprobe for the __x64_sys_bind function
 SEC("kprobe/__x64_sys_bind")
 /**
 * This is the implementation of kprobe to the x64_sys_bind function. This function is called when the system binds to a process.
@@ -28,11 +31,22 @@ SEC("kprobe/__x64_sys_bind")
 int kprobe_bind(struct pt_regs *ctx)
 {
     struct event_data args = {};
+        
+    // Extract the third argument from the pt_regs of the kprobe event
     struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1(ctx);
+
+    // Read the first argument of __x64_sys_bind and store it in args.
     bpf_probe_read(&args.args[0], sizeof(args.args[0]), &PT_REGS_PARM1(ctx2));
+    
+    // Read the second argument of __x64_sys_bind and store it in args.
     bpf_probe_read(&args.args[1], sizeof(args.args[1]), &PT_REGS_PARM2(ctx2));
+
+    // Output the event data to the perf event array
     bpf_perf_event_output(ctx, &event, BPF_F_CURRENT_CPU, &args, sizeof(args));
+
+    // Return 0 on success
     return 0;
 }
 
+// Define the license for the eBPF program
 char _license[] SEC("license") = "Dual MIT/GPL";
