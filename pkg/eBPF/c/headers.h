4bcd7e7702935a46d65cf5d14ce86da264262522
cravela <cravela@intelops.dev>
2023-08-10 16:17:11 -0500

    feat: refactored c code

---
+++ pkg/eBPF/c/headers.h
@@ -0,0 +1,12 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include "vmlinux.h"
+#include "bpf_tracing.h"
+#include "bpf_helpers.h"
+#include "bpf_core_read.h"
+
+#include "common/common.h"
+#include "common/constants.h"
+
+#endif
