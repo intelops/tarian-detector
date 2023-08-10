e4f6a99c9232898d0211bb2ced554406c8fa6e43
cravela <cravela@intelops.dev>
2023-08-10 16:14:14 -0500

    feat: added utils package

---
+++ pkg/utils/utils.go
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 Authors of Tarian & the Organization created Tarian
+
+package utils
+
+import "fmt"
+
+// converts [256][4096]uint8 to string.
+func Uint8ArrtoString(arr [256][4096]uint8) string {
+	var res_str string = ""
+
+	for _, el := range arr {
+		temp := Uint8toString(el[:])
+		if len(temp) == 0 {
+			continue
+		}
+
+		if res_str != "" {
+			res_str += " " + temp
+		} else {
+			res_str += temp
+		}
+	}
+
+	return res_str
+}
+
+// converts [256][4096]uint8 to []string.
+func Uint8ArrtoStringArr(arr [256][4096]uint8) []string {
+	var res_arr_str []string
+	for _, el := range arr {
+		temp := Uint8toString(el[:])
+		if len(temp) == 0 {
+			continue
+		}
+
+		res_arr_str = append(res_arr_str, temp)
+	}
+
+	return res_arr_str
+}
+
+// converts []uint8 to string
+func Uint8toString(arr []uint8) string {
+	return fmt.Sprintf("%s", removeNullBytes(arr)[:])
+}
+
+// removes null bytes from []uint8.
+func removeNullBytes(arr []uint8) []uint8 {
+	idx := indexOf(arr, 0)
+	return arr[:idx]
+}
+
+// returns the index of first occurence of element in the array
+func indexOf(arr []uint8, val uint8) int {
+	for idx, ele := range arr {
+		if ele == val {
+			return idx
+		}
+	}
+
+	return -1
+}
