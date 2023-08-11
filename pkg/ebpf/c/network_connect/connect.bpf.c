da66b0faf4ef88f6bd1e54a41171a186eede2689
AnamikaSanjay <asanjay@intelops.dev>
2023-08-11 16:42:54 -0500

    added IP and port fields~

--- pkg/ebpf/c/network_connect/connect.bpf.c
+++ pkg/ebpf/c/network_connect/connect.bpf.c
@@ -7,18 +7,45 @@
 #include "bpf_helpers.h"
 #include "bpf_core_read.h"
 
-// Define the structure for event data
+// Socket address family definitions
+#define AF_INET 2
+#define AF_INET6 10
+#define AF_UNIX 1
+#define MAX_UNIX_PATH 108  // standard size for UNIX paths
+
+// Define the structure for event data which contains process, user, and socket details
 struct event_data{
-    unsigned long args[3];
+    __u32 pid;  // Process ID
+    __u32 tgid; // Thread group ID
+    __u32 uid;  // User ID
+    __u32 gid;  // Group ID
+    int fd;     // File descriptor
+    int addrlen;// Address length
+    __u16 sa_family; // Socket address family
+    __u16 port;      // Port number
+    struct {
+        __be32 s_addr;  // IPv4 address
+    } v4_addr;
+    struct {
+        __u8 s6_addr[16]; // IPv6 address
+    } v6_addr;
+    struct {
+        char path[MAX_UNIX_PATH]; // UNIX socket path
+    } unix_addr;
+    __u32 padding; // Padding for alignment
 };
 const struct event_data *unused __attribute__((unused));
 
 // Define the ringbuff map with maximum 1 << 24 entries
 struct{
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 1 << 24);
+    __uint(max_entries, 1 << 24); // Maximum number of entries: 2^24
 } event SEC(".maps");
 
+// Utility function to convert port from network to host byte order
+static inline __u16 my_ntohs(__u16 port) {
+  return (port >> 8) | (port << 8);
+}
 // Define the kprobe for the __x64_sys_connect function
 SEC("kprobe/__x64_sys_connect")
 /**
@@ -26,24 +53,65 @@ SEC("kprobe/__x64_sys_connect")
 * This function is called when the system is about to connect. 
 */
 int kprobe_connect(struct pt_regs * ctx){
-    struct event_data args = {};
-    // Create a struct to store the event data
-    struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1(ctx);
-    
-    // Extract the first three arguments from the pt_regs of the kprobe event
-    bpf_probe_read(&args.args[0], sizeof(args.args[0]), &PT_REGS_PARM1(ctx2));
-    bpf_probe_read(&args.args[1], sizeof(args.args[1]), &PT_REGS_PARM2(ctx2));
-    bpf_probe_read(&args.args[2], sizeof(args.args[2]), &PT_REGS_PARM3(ctx2));
+   struct event_data *ed; // Pointer to hold the reserved space in the ring buffer
 
-    // Output the event data to the perf event array
-    struct event_data *task_info; // Pointer to hold the reserved space in the ring buffer
-    task_info = bpf_ringbuf_reserve(&event, sizeof(struct event_data), 0); // Reserve space in ring buffer for event data
-    if (!task_info) {
+    ed = bpf_ringbuf_reserve(&event, sizeof(struct event_data), 0); // Reserve space in ring buffer for event data
+    if (!ed) {
     return 0;
     }
-    *task_info = args; // Copy the extracted args into the reserved space
-    bpf_ringbuf_submit(task_info, 0); // Submit the event to the ring buffer for userspace to consume
-    return 0;
+     //Process Id and Thread Group Id
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    ed->pid = pid_tgid >> 32;
+    ed->tgid = pid_tgid;
+
+    //User Id and Group Id
+    __u64 uid_gid = bpf_get_current_uid_gid();
+    ed->uid = uid_gid >> 32;
+    ed->gid = uid_gid;
+    
+    // Extract the first two arguments from the pt_regs of the kprobe event
+   struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1_CORE(ctx); // Extract the first parameter
+    ed->fd=(int)PT_REGS_PARM1_CORE(ctx2);
+   struct sockaddr *uservaddr_ptr=(struct sockaddr *)PT_REGS_PARM2_CORE(ctx2);
+    ed->addrlen = (int)PT_REGS_PARM3_CORE(ctx2);
+    bpf_printk("addrlen: %d\n", ed->addrlen);
+
+    // Read the address family to determine socket type
+    bpf_probe_read_user(&ed->sa_family, sizeof(ed->sa_family), uservaddr_ptr);
+
+    // Parse data based on socket type
+    switch (ed->sa_family) {
+        case AF_INET:// For IPv4 addresses
+        {
+            struct sockaddr_in v4;
+            bpf_probe_read_user(&v4, sizeof(v4), uservaddr_ptr);
+            ed->v4_addr.s_addr = v4.sin_addr.s_addr;
+            ed->port = my_ntohs(v4.sin_port); // Convert from network to host byte order
+        }
+            break;
+        case AF_INET6:// For IPv6 addresses
+   {
+        struct sockaddr_in6 v6;
+        bpf_probe_read_user(&v6, sizeof(v6), uservaddr_ptr);
+        
+        // Copying the IPv6 address
+        #pragma unroll
+        for (int i = 0; i < 16; i++) {
+            ed->v6_addr.s6_addr[i] = v6.sin6_addr.in6_u.u6_addr8[i];
+        }
+        
+        // Reading the IPv6 port
+        ed->port = my_ntohs(v6.sin6_port); // Convert from network to host byte order
+    }
+            break;
+        case AF_UNIX:// For UNIX sockets
+            bpf_probe_read_user(&ed->unix_addr, sizeof(struct sockaddr_un), uservaddr_ptr);
+            break;
+    }
+
+    bpf_ringbuf_submit(ed, 0); // Submit the event to the ring buffer for userspace to consume
+    return 0; // Exit the kprobe
+
 }
 
 // Define the license for the BPF program
