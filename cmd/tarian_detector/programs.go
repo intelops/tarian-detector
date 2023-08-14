cf476479fb234af454b873afcdcdf47dadf5dd05
cravela <cravela@intelops.dev>
2023-08-13 20:22:59 -0500

    feat: added new detectors

--- cmd/tarian_detector/programs.go
+++ cmd/tarian_detector/programs.go
@@ -4,20 +4,28 @@
 package main
 
 import (
-	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/process_entry"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/file_close"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/file_read"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/file_readv"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/process_execve"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/BPF/process_execveat"
 	"github.com/intelops/tarian-detector/pkg/inspector/detector"
 	"github.com/intelops/tarian-detector/pkg/inspector/ebpf_manager"
 )
 
 // attaches the ebpf programs to kernel and returns the refrences of maps and link.
-func getEbpfPrograms() ([]detector.EventDetector, error) {
+func getEbpfDetectors() ([]detector.EventDetector, error) {
 
 	//holds reference to all ebpf programs
 	var ebpf_programs = []ebpf_manager.EbpfProgram{
-		process_entry.NewProcessEntryEbpf(),
+		process_execve.NewExecve(),
+		process_execveat.NewExecveat(),
+		file_read.NewRead(),
+		file_readv.NewReadv(),
+		file_close.NewClose(),
 	}
 
-	eBPFPrograms := ebpf_manager.NewEbpfProgram()
+	eBPFPrograms := ebpf_manager.NewEbpfPrograms()
 	for _, program := range ebpf_programs {
 		eBPFPrograms.Add(program)
 	}
