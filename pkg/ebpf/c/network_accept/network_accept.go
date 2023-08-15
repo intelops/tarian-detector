005947f8d7f4fb4a4d5ff9d34303ac25a984447a
AnamikaSanjay <asanjay@intelops.dev>
2023-08-14 19:59:17 -0500

    added event description for accept syscall and added IP , family and port interperation

--- pkg/ebpf/c/network_accept/network_accept.go
+++ pkg/ebpf/c/network_accept/network_accept.go
@@ -6,6 +6,10 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"net"
+	"strings"
+	"unsafe"
 
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/ringbuf"
@@ -14,6 +18,11 @@ import (
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags $BPF_CFLAGS -target $CURR_ARCH  -type event_data accept accept.bpf.c -- -I../../../../headers
 
 // getEbpfObject loads the eBPF objects from the compiled code and returns a pointer to the acceptObjects structure.
+const (
+    AF_INET  = 2   // Adjust with your actual values
+    AF_INET6 = 10
+    AF_UNIX  = 1
+)
 func getEbpfObject() (*acceptObjects, error) {
 	var bpfObj acceptObjects
 	// Load eBPF objects from the compiled  code into bpfObj.
@@ -31,7 +40,16 @@ func getEbpfObject() (*acceptObjects, error) {
 // The intention is to use the proper Go string instead of byte arrays from C.
 // It makes it simpler to use and can generate proper json.
 type AcceptEventData struct {
-	Args [3]uint64
+	Pid      uint32
+	Tgid     uint32
+	Uid      uint32
+	Gid      uint32
+	Fd       int32
+	SaFamily uint16
+	Port     uint16
+	V4Addr   struct{ S_addr uint32 }
+	V6Addr   struct{ S6Addr [16]uint8 }
+	UnixAddr struct{ Path [108]int8 }
 }
 
 // newAcceptEventDataFromEbpf converts an EBPF accept event to an AcceptEventData. 
@@ -39,12 +57,17 @@ type AcceptEventData struct {
 // @param e - the EBPF accept event to convert to an Accept
 func newAcceptEventDataFromEbpf(e acceptEventData) *AcceptEventData {
 	evt := &AcceptEventData{
-		Args: [3]uint64{
-			e.Args[0],
-			e.Args[1],
-			e.Args[2],
-		},
-	}
+			Pid:       e.Pid,
+			Tgid:      e.Tgid,
+			Uid:       e.Uid,
+			Gid:       e.Gid,
+			Fd:        e.Fd,
+			Port:      e.Port,		
+			SaFamily:  e.SaFamily,
+			V4Addr:    e.V4Addr,
+			V6Addr:    e.V6Addr,
+			UnixAddr:  e.UnixAddr,
+		}
 	return evt
 }
 
@@ -125,6 +148,51 @@ func (o *NetworkAcceptDetector) Read() (*AcceptEventData, error) {
 // It calls the Read method internally.
 func (o *NetworkAcceptDetector) ReadAsInterface() (any, error) {
 	return o.Read()
+}
+
+// Convert IPv4 address from binary to string.
+func ipv4ToString(addr uint32) string {
+    return fmt.Sprintf("%d.%d.%d.%d", byte(addr), byte(addr>>8), byte(addr>>16), byte(addr>>24))
+}
+
+
+// Convert IPv6 address from binary to string.
+func ipv6ToString(addr [16]uint8) string {
+    b := make([]byte, 16)
+    for i := 0; i < 4; i++ {
+        val := binary.BigEndian.Uint32(addr[i*4 : (i+1)*4])
+        binary.BigEndian.PutUint32(b[i*4:], val)
+    }
+    return net.IP(b).String()
+}
+
+
+
+func byteArrayToString(b [108]int8) string {
+    return strings.TrimRight(string((*[108]byte)(unsafe.Pointer(&b))[:]), "\x00")
+}
+
+func (e *AcceptEventData) InterpretPort() uint16 {
+ return e.Port
+}
+
+func (e *AcceptEventData) InterpretFamilyAndIP() (family string, ip string, port uint16) {
+    switch e.SaFamily {
+    case AF_INET:
+        family = "AF_INET"
+        ip = ipv4ToString(e.V4Addr.S_addr)
+    case AF_INET6:
+        family = "AF_INET6"
+        ip = ipv6ToString(e.V6Addr.S6Addr)
+    case AF_UNIX:
+        family = "AF_UNIX"
+		ip = byteArrayToString(e.UnixAddr.Path) // Read the path for UNIX socket.
+    default:
+        family = "UNKNOWN"
+        ip = "N/A"
+    }
+	port = e.InterpretPort() // Get the port
+    return family, ip ,port
 }
 
 
