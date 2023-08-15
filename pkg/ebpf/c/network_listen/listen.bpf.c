10e662214dfa562fdbbfc47f0d721e5924f798d4
AnamikaSanjay <asanjay@intelops.dev>
2023-08-15 16:21:09 -0500

    added event description

--- pkg/ebpf/c/network_listen/listen.bpf.c
+++ pkg/ebpf/c/network_listen/listen.bpf.c
@@ -9,7 +9,12 @@
 
 struct event_data
 {
-    unsigned long args[3];
+    __u32 pid;
+    __u32 tgid;
+    __u32 uid;
+    __u32 gid;
+    int fd;
+    int backlog; 
 };
 const struct event_data *unused __attribute__((unused));
 // Define the ringbuff map with maximum 1 << 24 entries
@@ -27,24 +32,26 @@ SEC("kprobe/__x64_sys_listen")
  */
 int kprobe_listen(struct pt_regs *ctx)
 {
-    struct event_data args = {};
-    
-    struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1(ctx);
-   
-    // Read the first argument of __x64_sys_listen and store it in args.
-    bpf_probe_read(&args.args[0], sizeof(args.args[0]), &PT_REGS_PARM1(ctx2));
-
-    // Read the second argument of __x64_sys_listen and store it in args.
-    bpf_probe_read(&args.args[1], sizeof(args.args[1]), &PT_REGS_PARM2(ctx2));
-
-    // Output the event data to the  event array
-    struct event_data *task_info; // Pointer to hold the reserved space in the ring buffer
-    task_info = bpf_ringbuf_reserve(&event, sizeof(struct event_data), 0); // Reserve space in ring buffer for event data
-    if (!task_info) {
+    struct event_data *ed; // Pointer to hold the reserved space in the ring buffer
+    ed = bpf_ringbuf_reserve(&event, sizeof(struct event_data), 0); // Reserve space in ring buffer for event data
+    if (!ed) {
     return 0;
     }
-    *task_info = args; // Copy the extracted args into the reserved space
-    bpf_ringbuf_submit(task_info, 0); // Submit the event to the ring buffer for userspace to consume
+        //Process Id and Thread Group Id
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    ed->pid = pid_tgid >> 32;
+    ed->tgid = pid_tgid;
+
+        //User Id and Group Id
+    __u64 uid_gid = bpf_get_current_uid_gid();
+    ed->uid = uid_gid >> 32;
+    ed->gid = uid_gid;
+
+    // Extract the first two arguments from the pt_regs of the kprobe event
+   struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1_CORE(ctx); // Extract the first parameter
+    ed->fd=(int)PT_REGS_PARM1_CORE(ctx2);
+    ed->backlog=(int)PT_REGS_PARM2_CORE(ctx2);
+    bpf_ringbuf_submit(ed, 0); // Submit the event to the ring buffer for userspace to consume
     return 0;
 }
 
