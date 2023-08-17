98bbefa0663d2578fc25ac07affc6857ca809fcd
cravela <cravela@intelops.dev>
2023-08-17 16:53:03 -0500

    feat: added new fields to context.

--- pkg/eBPF/c/common/types.h
+++ pkg/eBPF/c/common/types.h
@@ -3,8 +3,17 @@
 
 #include "constants.h"
 
+typedef struct {
+	__u8 sysname[65];
+	__u8 nodename[65];
+	__u8 release[65];
+	__u8 version[65];
+	__u8 machine[65];
+	__u8 domainname[65];
+} node_info_t;
+
 typedef struct __attribute__((__packed__)) event_context {
-  
+
   __u64 ts;         // boot time
   __u64 start_time; // start time
 
@@ -17,9 +26,10 @@ typedef struct __attribute__((__packed__)) event_context {
   __u32 uid; // user id
   __u32 gid; // group id
 
-  __u8 nodename[65];        // nodename
   __u8 comm[TASK_COMM_LEN]; // command
   __u8 cwd[32];             // current working directory
+  
+  node_info_t node_info;  // node/system information
 } event_context_t;
 
 typedef struct args {
