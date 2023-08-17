538f1765035941b8250a34d9719d9b8789444c9c
cravela <cravela@intelops.dev>
2023-08-17 16:55:45 -0500

    feat: updated code to get context

--- pkg/eBPF/c/common/context.h
+++ pkg/eBPF/c/common/context.h
@@ -5,36 +5,37 @@
 #include "types.h"
 #include "task.h"
 
-static __always_inline int set_context(event_context_t *e_ctx) {
+static __always_inline int set_context(event_context_t *eventContext) {
 
   struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 
   // system boot time
-  e_ctx->ts = bpf_ktime_get_ns();
+  eventContext->ts = bpf_ktime_get_ns();
 
   // thread start time 
-  e_ctx->start_time = get_task_start_time(task);
+  eventContext->start_time = get_task_start_time(task);
 
   // process Id and thread Group Id
-  get_pid_tgid(&e_ctx->pid, &e_ctx->tgid);
+  get_pid_tgid(&eventContext->pid, &eventContext->tgid);
 
   // real_parent process id
-  e_ctx->ppid = get_task_ppid(task);
+  eventContext->ppid = get_task_ppid(task);
   
   // group leader process id
-  e_ctx->glpid = get_task_glpid(task);
+  eventContext->glpid = get_task_glpid(task);
 
   // user Id and group Id
-  get_uid_gid(&e_ctx->uid, &e_ctx->gid);
+  get_uid_gid(&eventContext->uid, &eventContext->gid);
 
   // nodename
-  BPF_READ_STR(get_task_node_name(task), &e_ctx->nodename);
+  // BPF_READ_STR(get_task_node_name(task), &eventContext->nodename);
+  get_node_info(&eventContext->node_info);
 
   // Command trigred event
-  BPF_GET_COMM(e_ctx->comm);
+  BPF_GET_COMM(eventContext->comm);
 
   // current working directory
-  __s64 res = get_cwd(&e_ctx->cwd);
+  __s64 res = get_cwd(&eventContext->cwd);
   if (res < 0) {
     return -1;
   }
