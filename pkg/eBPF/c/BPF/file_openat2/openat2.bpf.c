bb0eaec453e39ac4f59ac904edee179c0e04b28c
cravela <cravela@intelops.dev>
2023-08-22 21:28:45 -0500

    feat: update code refactor

--- pkg/eBPF/c/BPF/file_openat2/openat2.bpf.c
+++ pkg/eBPF/c/BPF/file_openat2/openat2.bpf.c
@@ -3,7 +3,7 @@
 
 //go:build ignore
 
-#include "headers.h"
+#include "includes.h"
 
 // data gathered by this program
 struct event_data {
@@ -38,29 +38,30 @@ int kprobe_openat2_entry(struct pt_regs *ctx) {
   ed->id = 0;
 
   // sets the context
-  set_context(&ed->eventContext);
+  init_context(&ed->eventContext);
 
-  struct pt_regs *ctx2 = (struct pt_regs *)PT_REGS_PARM1_CORE(ctx);
+  sys_args_t sys_args;
+  read_sys_args_into(&sys_args, ctx);
 
   // file descriptor
-  ed->fd = (int)PT_REGS_PARM1_CORE(ctx2);
+  ed->fd = (int)sys_args[0];
 
   // filename
-  long res = BPF_READ_STR((char *)PT_REGS_PARM2_CORE(ctx2), &ed->filename);
+  long res = BPF_READ_STR((char *)sys_args[1], &ed->filename);
   if (res < 0) {
     BPF_RINGBUF_DISCARD(ed);
     return -1;
   }
   
   // open how struct
-  res = BPF_READ_STR((struct open_how *)PT_REGS_PARM3_CORE(ctx2), &ed->how);
+  res = BPF_READ_STR((struct open_how *)sys_args[2], &ed->how);
   if(res < 0) {
     BPF_RINGBUF_DISCARD(ed);
     return -1;
   }
 
   // usize
-  ed->usize = (long unsigned int)PT_REGS_PARM4_CORE(ctx2);
+  ed->usize = (long unsigned int)sys_args[3];
 
   // pushes the information to ringbuf event mamp
   BPF_RINGBUF_SUBMIT(ed);
@@ -82,7 +83,7 @@ int kretprobe_openat2_exit(struct pt_regs *ctx) {
   ed->id = 1;
 
   // sets the context
-  set_context(&ed->eventContext);
+  init_context(&ed->eventContext);
 
   // return value - long
   ed->ret = (long)PT_REGS_RC_CORE(ctx);
