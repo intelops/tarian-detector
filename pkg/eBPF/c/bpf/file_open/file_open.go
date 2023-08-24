99947e3d5d4fd95e7400e5abbe9a40eace54aa23
cravela <cravela@intelops.dev>
2023-08-24 16:15:02 -0500

    feat: code refactor

--- pkg/eBPF/c/bpf/file_open/file_open.go
+++ pkg/eBPF/c/bpf/file_open/file_open.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/ebpf/link"
-	"github.com/intelops/tarian-detector/pkg/inspector/ebpf_manager"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/bpf"
 	"github.com/intelops/tarian-detector/pkg/utils"
 )
 
@@ -19,44 +19,45 @@ func NewFileOpen() *FileOpen {
 	return &FileOpen{}
 }
 
-func (fo *FileOpen) NewEbpf() (ebpf_manager.EbpfModule, error) {
-	var em ebpf_manager.EbpfModule
+func (fo *FileOpen) NewModule() (bpf.BpfModule, error) {
+	bm := bpf.NewBpfModule()
 
 	bpfObjs, err := getEbpfObject()
 	if err != nil {
-		return em, err
+		return bm, err
 	}
 
-	em.Programs = []ebpf_manager.Program{
+	bm.Programs = []bpf.BpfProgram{
 		{
 			Id: "__x64_sys_open_entry",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kprobe,
 				Name: "__x64_sys_open",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KprobeOpenEntry,
+			Name:         bpfObjs.KprobeOpenEntry,
 			ShouldAttach: true,
 		},
 		{
 			Id: "__x64_sys_open_exit",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kretprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kretprobe,
 				Name: "__x64_sys_open",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KretprobeOpenExit,
+			Name:         bpfObjs.KretprobeOpenExit,
 			ShouldAttach: true,
 		},
 	}
 
-	em.Data = &openEventData{}
-	em.Map = bpfObjs.Event
+	bm.Data = &openEventData{}
+	bm.Map = bpfObjs.Event
+	bm.ParseData = parseData
 
-	return em, nil
+	return bm, nil
 }
 
-func (fo *FileOpen) DataParser(data any) (map[string]any, error) {
+func parseData(data any) (map[string]any, error) {
 	event_data, ok := data.(*openEventData)
 	if !ok {
 		return nil, fmt.Errorf("type mismatch: expected %T received %T", event_data, data)
@@ -67,14 +68,14 @@ func (fo *FileOpen) DataParser(data any) (map[string]any, error) {
 	// event specific information
 	switch event_data.Id {
 	case 0:
-		res_data["id"] = "sys_open_entry"
+		res_data["id"] = "__x64_sys_open_entry"
 
 		res_data["filename"] = utils.Uint8toString(event_data.Filename[:])
 		res_data["flags"] = event_data.Flags
 		res_data["mode"] = event_data.Mode
 
 	case 1:
-		res_data["id"] = "sys_open_exit"
+		res_data["id"] = "__x64_sys_open_exit"
 
 		res_data["return_value"] = event_data.Ret
 
