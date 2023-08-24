e34cab70c4e48d6b4b3f3001cd1fd33b54a11bd8
cravela <cravela@intelops.dev>
2023-08-24 16:16:27 -0500

    feat: code refactor

--- pkg/eBPF/c/bpf/file_writev/file_writev.go
+++ pkg/eBPF/c/bpf/file_writev/file_writev.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/ebpf/link"
-	"github.com/intelops/tarian-detector/pkg/inspector/ebpf_manager"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/bpf"
 	"github.com/intelops/tarian-detector/pkg/utils"
 )
 
@@ -19,44 +19,45 @@ func NewFileWritev() *FileWritev {
 	return &FileWritev{}
 }
 
-func (ep *FileWritev) NewEbpf() (ebpf_manager.EbpfModule, error) {
-	var em ebpf_manager.EbpfModule
+func (fwv *FileWritev) NewModule() (bpf.BpfModule, error) {
+	bm := bpf.NewBpfModule()
 
 	bpfObjs, err := getEbpfObject()
 	if err != nil {
-		return em, err
+		return bm, err
 	}
 
-	em.Programs = []ebpf_manager.Program{
+	bm.Programs = []bpf.BpfProgram{
 		{
 			Id: "__x64_sys_writev_entry",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kprobe,
 				Name: "__x64_sys_writev",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KprobeWritevEntry,
+			Name:         bpfObjs.KprobeWritevEntry,
 			ShouldAttach: true,
 		},
 		{
 			Id: "__x64_sys_writev_exit",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kretprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kretprobe,
 				Name: "__x64_sys_writev",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KretprobeWritevExit,
+			Name:         bpfObjs.KretprobeWritevExit,
 			ShouldAttach: true,
 		},
 	}
 
-	em.Data = &writevEventData{}
-	em.Map = bpfObjs.Event
+	bm.Data = &writevEventData{}
+	bm.Map = bpfObjs.Event
+	bm.ParseData = parseData
 
-	return em, nil
+	return bm, nil
 }
 
-func (ep *FileWritev) DataParser(data any) (map[string]any, error) {
+func parseData(data any) (map[string]any, error) {
 	event_data, ok := data.(*writevEventData)
 	if !ok {
 		return nil, fmt.Errorf("type mismatch: expected %T received %T", event_data, data)
@@ -67,13 +68,13 @@ func (ep *FileWritev) DataParser(data any) (map[string]any, error) {
 	// event specific information
 	switch event_data.Id {
 	case 0:
-		res_data["id"] = "sys_writev_entry"
+		res_data["id"] = "__x64_sys_writev_entry"
 
 		res_data["file_descriptor"] = event_data.Fd
 		res_data["vlen"] = event_data.Vlen
 
 	case 1:
-		res_data["id"] = "sys_writev_exit"
+		res_data["id"] = "__x64_sys_writev_exit"
 
 		res_data["return_value"] = event_data.Ret
 
