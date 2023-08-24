e83cd5905c4e14642ee616afa3bea709091179fa
cravela <cravela@intelops.dev>
2023-08-24 16:16:51 -0500

    feat: code refactor

--- pkg/eBPF/c/bpf/process_execveat/process_execveat.go
+++ pkg/eBPF/c/bpf/process_execveat/process_execveat.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 
 	"github.com/cilium/ebpf/link"
-	"github.com/intelops/tarian-detector/pkg/inspector/ebpf_manager"
+	"github.com/intelops/tarian-detector/pkg/eBPF/c/bpf"
 	"github.com/intelops/tarian-detector/pkg/utils"
 )
 
@@ -15,50 +15,51 @@ import (
 
 type ProcessExecveat struct{}
 
-func NewExecveat() *ProcessExecveat {
+func NewProcessExecveat() *ProcessExecveat {
 	return &ProcessExecveat{}
 }
 
 // Tells how to create a ebpf program
-func (pe *ProcessExecveat) NewEbpf() (ebpf_manager.EbpfModule, error) {
-	var em ebpf_manager.EbpfModule
+func (pe *ProcessExecveat) NewModule() (bpf.BpfModule, error) {
+	bm := bpf.NewBpfModule()
 
 	bpfObjs, err := getEbpfObject()
 	if err != nil {
-		return em, err
+		return bm, err
 	}
 
-	em.Programs = []ebpf_manager.Program{
+	bm.Programs = []bpf.BpfProgram{
 		{
 			Id: "__x64_sys_execveat_entry",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kprobe,
 				Name: "__x64_sys_execveat",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KprobeExecveatEntry,
+			Name:         bpfObjs.KprobeExecveatEntry,
 			ShouldAttach: true,
 		},
 		{
 			Id: "__x64_sys_execveat_exit",
-			Hook: ebpf_manager.Hook{
-				Type: ebpf_manager.Kretprobe,
+			Hook: bpf.Hook{
+				Type: bpf.Kretprobe,
 				Name: "__x64_sys_execveat",
 				Opts: &link.KprobeOptions{}, //can be nil
 			},
-			Program:      bpfObjs.KretprobeExecveatExit,
+			Name:         bpfObjs.KretprobeExecveatExit,
 			ShouldAttach: true,
 		},
 	}
 
-	em.Data = &execveatEventData{}
-	em.Map = bpfObjs.Event
+	bm.Data = &execveatEventData{}
+	bm.Map = bpfObjs.Event
+	bm.ParseData = parseData
 
-	return em, nil
+	return bm, nil
 }
 
 // Tells how to parse the information received from the ebpf program
-func (pe *ProcessExecveat) DataParser(data any) (map[string]any, error) {
+func parseData(data any) (map[string]any, error) {
 	event_data, ok := data.(*execveatEventData)
 	if !ok {
 		return nil, fmt.Errorf("type mismatch: expected %T received %T", event_data, data)
@@ -69,7 +70,7 @@ func (pe *ProcessExecveat) DataParser(data any) (map[string]any, error) {
 	// event specific information
 	switch event_data.Id {
 	case 0:
-		res_data["id"] = "sys_execveat_entry"
+		res_data["id"] = "__x64_sys_execveat_entry"
 
 		res_data["file_descriptor"] = event_data.Fd
 		res_data["binary_file_path"] = utils.Uint8toString(event_data.BinaryFilepath[:])
@@ -78,7 +79,7 @@ func (pe *ProcessExecveat) DataParser(data any) (map[string]any, error) {
 		res_data["flags"] = event_data.Flags
 
 	case 1:
-		res_data["id"] = "sys_execveat_exit"
+		res_data["id"] = "__x64_sys_execveat_exit"
 
 		res_data["return_value"] = event_data.Ret
 
