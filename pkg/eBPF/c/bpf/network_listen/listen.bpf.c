125897d2cbcd317a78939061aee39e1f0c049eb2
cravela <cravela@intelops.dev>
2023-08-27 08:56:55 -0500

    feat: update map name

--- pkg/eBPF/c/bpf/network_listen/listen.bpf.c
+++ pkg/eBPF/c/bpf/network_listen/listen.bpf.c
@@ -5,12 +5,12 @@
 
 #include "includes.h"
 
-// Struct representing the event data captured by this program
+// Struct representing the listen_event_map data captured by this program
 struct event_data {
-  event_context_t eventContext; // Context for the event
+  event_context_t eventContext; // Context for the listen_event_map
 
-  int id;      // Identifier for the event type (entry or exit)
-  int fd;      // File descriptor associated with the event
+  int id;      // Identifier for the listen_event_map type (entry or exit)
+  int fd;      // File descriptor associated with the listen_event_map
   int backlog; // Queue length for completely established sockets waiting to be
                // accepted
   int ret;     // Return value, used for capturing syscall exit values
@@ -20,23 +20,23 @@ struct event_data {
 // even if it seems unused
 const struct event_data *unused __attribute__((unused));
 
-// Defines a ringbuffer map named 'event'
-BPF_RINGBUF_MAP(event);
+// Defines a ringbuffer map named 'listen_event_map'
+BPF_RINGBUF_MAP(listen_event_map);
 
 // eBPF program attached to the entry of the '__x64_sys_listen' kernel function
 SEC("kprobe/__x64_sys_listen")
 int kprobe_listen_entry(struct pt_regs *ctx) {
   struct event_data *ed;
 
-  // Reserve space for an event in the ringbuffer map
-  ed = BPF_RINGBUF_RESERVE(event, *ed);
+  // Reserve space for an listen_event_map in the ringbuffer map
+  ed = BPF_RINGBUF_RESERVE(listen_event_map, *ed);
   if (!ed) {
     return -1; // Return if space allocation failed
   }
 
-  ed->id = 0; // Set event type to 'entry'
+  ed->id = 0; // Set listen_event_map type to 'entry'
 
-  // Initialize event context
+  // Initialize listen_event_map context
   init_context(&ed->eventContext);
 
   sys_args_t sys_args;
@@ -49,7 +49,7 @@ int kprobe_listen_entry(struct pt_regs *ctx) {
   // Extract and store the backlog value from syscall arguments
   ed->backlog = (int)sys_args[1];
 
-  // Submit the captured event data to the ringbuffer map
+  // Submit the captured listen_event_map data to the ringbuffer map
   BPF_RINGBUF_SUBMIT(ed);
 
   return 0;
@@ -60,21 +60,21 @@ SEC("kretprobe/__x64_sys_listen")
 int kretprobe_listen_exit(struct pt_regs *ctx) {
   struct event_data *ed;
 
-  // Reserve space for an event in the ringbuffer map
-  ed = BPF_RINGBUF_RESERVE(event, *ed);
+  // Reserve space for an listen_event_map in the ringbuffer map
+  ed = BPF_RINGBUF_RESERVE(listen_event_map, *ed);
   if (!ed) {
     return -1; // Return if space allocation failed
   }
 
-  ed->id = 1; // Set event type to 'exit'
+  ed->id = 1; // Set listen_event_map type to 'exit'
 
-  // Initialize event context
+  // Initialize listen_event_map context
   init_context(&ed->eventContext);
 
   // Capture the return value of the syscall
   ed->ret = (int)PT_REGS_RC_CORE(ctx);
 
-  // Submit the captured event data to the ringbuffer map
+  // Submit the captured listen_event_map data to the ringbuffer map
   BPF_RINGBUF_SUBMIT(ed);
 
   return 0;
