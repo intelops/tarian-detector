0281f77f98d6abb41a9fa0512982e4a8d0562626
cravela <cravela@intelops.dev>
2023-09-07 13:29:00 -0500

    feat: rename field name

--- pkg/eBPF/c/bpf/file_close/file_close.go
+++ pkg/eBPF/c/bpf/file_close/file_close.go
@@ -64,19 +64,19 @@ func parseData(data any) (map[string]any, error) {
 	}
 
 	res_data := utils.SetContext(event_data.EventContext)
+	res_data["tarian_detector"] = "file_close"
 
 	// event specific information
 	switch event_data.Id {
 	case 0:
-		res_data["tarian_detector"] = "__x64_sys_close_entry"
+		res_data["tarian_detector_hook"] = "__x64_sys_close_entry"
 
 		res_data["file_descriptor"] = event_data.Fd
 
 	case 1:
-		res_data["tarian_detector"] = "__x64_sys_close_exit"
+		res_data["tarian_detector_hook"] = "__x64_sys_close_exit"
 
 		res_data["return_value"] = event_data.Ret
-
 	}
 
 	return res_data, nil
