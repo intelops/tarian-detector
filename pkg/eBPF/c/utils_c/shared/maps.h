c0a8072f5a54777d89760a21f874120d52132156
cravela <cravela@intelops.dev>
2023-11-27 10:56:27 -0600

    feat: add new maps

--- pkg/eBPF/c/utils_c/shared/maps.h
+++ pkg/eBPF/c/utils_c/shared/maps.h
@@ -26,7 +26,7 @@
 #define BPF_RINGBUF_MAP(__name__)                                              \
   struct {                                                                     \
     __uint(type, BPF_MAP_TYPE_RINGBUF);                                        \
-    __uint(max_entries, RINGBUF_MAX_ENTRIES);                                              \
+    __uint(max_entries, RINGBUF_MAX_ENTRIES);                                   \
   } __name__ SEC(".maps");
 
 // Ringbuf helpers
@@ -39,5 +39,46 @@
 BPF_RINGBUF_MAP(EVENT_RINGBUF_MAP_NAME);
 // BPF_PERCPU_ARRAY(heap, sizeof(struct event_data), 1);
 // BPF_PERF_EVENT_ARRAY(perf_events, int, sizeof(u32));
+
+
+struct ringbuffer{                                                                     
+__uint(type, BPF_MAP_TYPE_RINGBUF);                                       
+__uint(max_entries, 16 * 1024 * 1024);                                   
+} rb_cpu0 SEC(".maps"), rb_cpu1 SEC(".maps"), rb_cpu2 SEC(".maps"), rb_cpu3 SEC(".maps"), rb_cpu4 SEC(".maps"),
+rb_cpu5 SEC(".maps"), rb_cpu6 SEC(".maps"), rb_cpu7 SEC(".maps"), rb_cpu8 SEC(".maps"),
+rb_cpu9 SEC(".maps"), rb_cpu10 SEC(".maps"), rb_cpu11 SEC(".maps"), rb_cpu12 SEC(".maps"),
+rb_cpu13 SEC(".maps"), rb_cpu14 SEC(".maps"), rb_cpu15 SEC(".maps");
+
+/**
+ * This array of maps will contain a variable number of ring buffers
+ * according to the user-provided configuration. It could also contain only
+ * one buffer shared between all CPUs. 
+ */
+struct
+{
+	__uint(type, BPF_MAP_TYPE_ARRAY_OF_MAPS);
+  __uint(max_entries, 16);                                   
+	__type(key, uint32_t);
+	__array(values, struct ringbuffer);
+} percpu_rb SEC(".maps") = {
+  .values = {
+    &rb_cpu0,
+    &rb_cpu1,
+    &rb_cpu2,
+    &rb_cpu3,
+    &rb_cpu4,
+    &rb_cpu5,
+    &rb_cpu6,
+    &rb_cpu7,
+    &rb_cpu8,
+    &rb_cpu9,
+    &rb_cpu10,
+    &rb_cpu11,
+    &rb_cpu12,
+    &rb_cpu13,
+    &rb_cpu14,
+    &rb_cpu15,
+  }
+};
 
 #endif
