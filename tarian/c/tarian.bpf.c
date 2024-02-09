66dbf91742dae6bf720781273aa17f190a1ec3f1
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-09 16:41:40 -0600

    feat: add stats collector

--- tarian/c/tarian.bpf.c
+++ tarian/c/tarian.bpf.c
@@ -9,7 +9,10 @@ KPROBE("__x64_sys_execve")
 int BPF_KPROBE(tdf_execve_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_EXECVE_E, &te, VARIABLE, TDS_EXECVE_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_flex_save(&te, TDT_STR, get_syscall_param(regs, 0) /* filename */, 0,USER);
@@ -24,7 +27,10 @@ KRETPROBE("__x64_sys_execve")
 int BPF_KRETPROBE(tdf_execve_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_EXECVE_R, &te, FIXED, TDS_EXECVE_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -37,7 +43,10 @@ KPROBE("__x64_sys_execveat")
 int BPF_KPROBE(tdf_execveat_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_EXECVEAT_E, &te, VARIABLE, TDS_EXECVEAT_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -58,7 +67,10 @@ KRETPROBE("__x64_sys_execveat")
 int BPF_KRETPROBE(tdf_execveat_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_EXECVEAT_R, &te, FIXED, TDS_EXECVEAT_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -71,7 +83,10 @@ KPROBE("__x64_sys_clone")
 int BPF_KPROBE(tdf_clone_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CLONE_E, &te, FIXED, TDS_CLONE_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   uint64_t flags = get_syscall_param(regs, 0);
@@ -99,7 +114,10 @@ KRETPROBE("__x64_sys_clone")
 int BPF_KRETPROBE(tdf_clone_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CLONE_R, &te, FIXED, TDS_CLONE_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -112,7 +130,10 @@ KPROBE("__x64_sys_close")
 int BPF_KPROBE(tdf_close_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CLOSE_E, &te, FIXED, TDS_CLOSE_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
   
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -126,7 +147,10 @@ KRETPROBE("__x64_sys_close")
 int BPF_KRETPROBE(tdf_close_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CLOSE_R, &te, FIXED, TDS_CLOSE_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -139,7 +163,10 @@ KPROBE("__x64_sys_read")
 int BPF_KPROBE(tdf_read_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_READ_E, &te, VARIABLE, TDS_READ_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int32_t fd = get_syscall_param(regs, 0);
@@ -158,7 +185,10 @@ KRETPROBE("__x64_sys_read")
 int BPF_KRETPROBE(tdf_read_r, long ret) { 
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_READ_R, &te, FIXED, TDS_READ_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S64, &ret);
@@ -171,7 +201,10 @@ KPROBE("__x64_sys_write")
 int BPF_KPROBE(tdf_write_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_WRITE_E, &te, VARIABLE, TDS_WRITE_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
   
   /*====================== PARAMETERS ======================*/
   int32_t fd = get_syscall_param(regs, 0);
@@ -190,7 +223,10 @@ KRETPROBE("__x64_sys_write")
 int BPF_KRETPROBE(tdf_write_r, long ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_WRITE_R, &te, FIXED, TDS_WRITE_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S64, &ret);
@@ -203,7 +239,10 @@ KPROBE("__x64_sys_open")
 int BPF_KPROBE(tdf_open_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPEN_E, &te, VARIABLE, TDS_OPEN_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_flex_save(&te, TDT_STR, get_syscall_param(regs, 0), 0, USER);
@@ -222,7 +261,10 @@ KRETPROBE("__x64_sys_open")
 int BPF_KRETPROBE(tdf_open_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPEN_R, &te, FIXED, TDS_OPEN_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_U32, &ret);
@@ -235,7 +277,10 @@ KPROBE("__x64_sys_readv")
 int BPF_KPROBE(tdf_readv_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_READV_E, &te, VARIABLE, TDS_READV_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -255,7 +300,10 @@ KRETPROBE("__x64_sys_readv")
 int BPF_KRETPROBE(tdf_readv_r,  long ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_READV_R, &te, FIXED, TDS_READV_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
   
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S64, &ret);
@@ -269,7 +317,10 @@ KPROBE("__x64_sys_writev")
 int BPF_KPROBE(tdf_writev_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_WRITEV_E, &te, VARIABLE, TDS_WRITEV_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -288,7 +339,10 @@ KRETPROBE("__x64_sys_writev")
 int BPF_KRETPROBE(tdf_writev_r,  long ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_WRITEV_R, &te, FIXED, TDS_WRITEV_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
   
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S64, &ret);
@@ -301,7 +355,10 @@ KPROBE("__x64_sys_openat")
 int BPF_KPROBE(tdf_openat_e, struct pt_regs  *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPENAT_E, &te, VARIABLE, TDS_OPENAT_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int dfd = get_syscall_param(regs, 0);
@@ -323,7 +380,10 @@ KRETPROBE("__x64_sys_openat")
 int BPF_KRETPROBE(tdf_openat_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPENAT_R, &te, FIXED, TDS_OPENAT_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32,  &ret);
@@ -336,7 +396,10 @@ KPROBE("__x64_sys_openat2")
 int BPF_KPROBE(tdf_openat2_e, struct pt_regs  *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPENAT2_E, &te, VARIABLE, TDS_OPENAT2_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int dfd = get_syscall_param(regs, 0);
@@ -362,7 +425,10 @@ KRETPROBE("__x64_sys_openat2")
 int BPF_KRETPROBE(tdf_openat2_r, long ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_OPENAT2_R, &te, FIXED, TDS_OPENAT2_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S64,  &ret);
@@ -375,7 +441,10 @@ KPROBE("__x64_sys_listen")
 int BPF_KPROBE(tdf_listen_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_LISTEN_E, &te, FIXED, TDS_LISTEN_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -392,7 +461,10 @@ KRETPROBE("__x64_sys_listen")
 int BPF_KRETPROBE(tdf_listen_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_LISTEN_R, &te, FIXED, TDS_LISTEN_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32,  &ret);
@@ -405,7 +477,10 @@ KPROBE("__x64_sys_socket")
 int BPF_KPROBE(tdf_socket_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_SOCKET_E, &te, FIXED, TDS_SOCKET_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int family = get_syscall_param(regs, 0);
@@ -425,7 +500,10 @@ KRETPROBE("__x64_sys_socket")
 int BPF_KRETPROBE(tdf_socket_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_SOCKET_R, &te, FIXED, TDS_SOCKET_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32,  &ret);
@@ -438,7 +516,10 @@ KPROBE("__x64_sys_accept")
 int BPF_KPROBE(tdf_accept_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_ACCEPT_E, &te, VARIABLE,  TDS_ACCEPT_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -459,7 +540,10 @@ KRETPROBE("__x64_sys_accept")
 int BPF_KRETPROBE(tdf_accept_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_ACCEPT_R, &te, FIXED,  TDS_ACCEPT_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -472,7 +556,10 @@ KPROBE("__x64_sys_bind")
 int BPF_KPROBE(tdf_bind_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_BIND_E, &te, VARIABLE,  TDS_BIND_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -491,7 +578,10 @@ KRETPROBE("__x64_sys_bind")
 int BPF_KRETPROBE(tdf_bind_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_BIND_R, &te, FIXED,  TDS_BIND_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
@@ -504,7 +594,10 @@ KPROBE("__x64_sys_connect")
 int BPF_KPROBE(tdf_connect_e, struct pt_regs *regs) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CONNECT_E, &te, VARIABLE,  TDS_CONNECT_E);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   int fd = get_syscall_param(regs, 0);
@@ -523,7 +616,10 @@ KRETPROBE("__x64_sys_connect")
 int BPF_KRETPROBE(tdf_connect_r, int ret) {
   tarian_event_t te;
   int resp = new_event(ctx, TDE_SYSCALL_CONNECT_R, &te, FIXED,  TDS_CONNECT_R);
-  if (resp != TDC_SUCCESS) return resp;
+  if (resp != TDC_SUCCESS) {
+    stats__add(resp);
+    return resp;
+  }
 
   /*====================== PARAMETERS ======================*/
   tdf_save(&te, TDT_S32, &ret);
