9f39a545d906b3c7554145d69e3831f26508ee3e
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-09 21:54:33 -0600

    refactor: removed unwanted code

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,11 +5,9 @@ package utils
 
 import (
 	"encoding/binary"
-	"encoding/json"
 	"net"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 )
 
@@ -63,35 +61,4 @@ func Ntohs(n uint16) uint16 {
 	binary.LittleEndian.PutUint16(b, n)
 
 	return binary.BigEndian.Uint16(b)
-}
-
-// WriteJSONToFile appends a JSON object to a file in JSON array format
-func WriteJSONToFile(data map[string]interface{}, filename string, mutex *sync.Mutex) error {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	var objects []map[string]interface{}
-
-	// Read existing JSON from the file, if it exists
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0600)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	err = json.NewDecoder(file).Decode(&objects)
-	if err != nil && err.Error() != "EOF" {
-		return err
-	}
-
-	// Append the new JSON object to the array
-	objects = append(objects, data)
-
-	// Write the updated JSON array to the file
-	file.Seek(0, 0)
-	file.Truncate(0)
-
-	encoder := json.NewEncoder(file)
-
-	return encoder.Encode(objects)
 }
