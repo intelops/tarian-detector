8a4ff75fe31c4ffcef2db077e67a36a067f1437b
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-15 15:38:24 -0600

    refactor: socket code optimized

--- pkg/eventparser/transform.go
+++ pkg/eventparser/transform.go
@@ -10,7 +10,7 @@ import (
 	"github.com/intelops/tarian-detector/pkg/err"
 )
 
-var tansformerErr = err.New("eventparser.transformer")
+var transformErr = err.New("eventparser.transform")
 
 const (
 	AT_FDCWD              = -100
@@ -35,7 +35,7 @@ var execveatDird = map[int32]string{
 func parseExecveatDird(dird any) (string, error) {
 	d, ok := dird.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", dird), tansformerErr.Throwf("parseExecveatDird: parse value error expected %T received %T", d, dird)
+		return fmt.Sprintf("%v", dird), transformErr.Throwf("parseExecveatDird: parse value error expected %T received %T", d, dird)
 	}
 
 	if s, ok := execveatDird[d]; ok {
@@ -57,7 +57,7 @@ var execveatFlag = []struct {
 func parseExecveatFlags(flag any) (string, error) {
 	f, ok := flag.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", flag), tansformerErr.Throwf("parseExecveatFlags: parse value error expected %T received %T", f, flag)
+		return fmt.Sprintf("%v", flag), transformErr.Throwf("parseExecveatFlags: parse value error expected %T received %T", f, flag)
 	}
 
 	var fs []string
@@ -133,7 +133,7 @@ var cloneFlags = map[uint64]string{
 func parseCloneFlags(flag any) (string, error) {
 	f, ok := flag.(uint64)
 	if !ok {
-		return fmt.Sprintf("%v", flag), tansformerErr.Throwf("parseCloneFlags: parse value error expected %T received %T", f, flag)
+		return fmt.Sprintf("%v", flag), transformErr.Throwf("parseCloneFlags: parse value error expected %T received %T", f, flag)
 	}
 
 	var fs []string
@@ -206,7 +206,7 @@ func parseSignal(sig uint16) (string, error) {
 func parseOpenMode(mode any) (string, error) {
 	m, ok := mode.(uint32)
 	if !ok {
-		return fmt.Sprintf("%v", mode), tansformerErr.Throwf("parseOpenMode: parse value error expected %T received %T", m, mode)
+		return fmt.Sprintf("%v", mode), transformErr.Throwf("parseOpenMode: parse value error expected %T received %T", m, mode)
 	}
 
 	return fmt.Sprintf("%04o", m), nil
@@ -260,7 +260,7 @@ var openFlags = map[int32]string{
 func parseOpenFlags(flags any) (string, error) {
 	f, ok := flags.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", f), tansformerErr.Throwf("parseOpenFlags: parse value error expected %T received %T", f, flags)
+		return fmt.Sprintf("%v", f), transformErr.Throwf("parseOpenFlags: parse value error expected %T received %T", f, flags)
 	}
 
 	var fs []string
@@ -285,7 +285,7 @@ func parseOpenFlags(flags any) (string, error) {
 func parseOpenat2Flags(flags any) (string, error) {
 	f, ok := flags.(int64)
 	if !ok {
-		return fmt.Sprintf("%v", f), tansformerErr.Throwf("parseOpenat2Flags: parse value error expected %T received %T", f, flags)
+		return fmt.Sprintf("%v", f), transformErr.Throwf("parseOpenat2Flags: parse value error expected %T received %T", f, flags)
 	}
 
 	return parseOpenFlags(int32(f))
@@ -294,7 +294,7 @@ func parseOpenat2Flags(flags any) (string, error) {
 func parseOpenat2Mode(mode any) (string, error) {
 	m, ok := mode.(int64)
 	if !ok {
-		return fmt.Sprintf("%v", m), tansformerErr.Throwf("parseOpenat2Mode: parse value error expected %T received %T", m, mode)
+		return fmt.Sprintf("%v", m), transformErr.Throwf("parseOpenat2Mode: parse value error expected %T received %T", m, mode)
 	}
 
 	return parseOpenMode(uint32(m))
@@ -321,7 +321,7 @@ var openat2ResolveFlags = map[int64]string{
 func parseOpenat2Resolve(resovle any) (string, error) {
 	r, ok := resovle.(int64)
 	if !ok {
-		return fmt.Sprintf("%v", r), tansformerErr.Throwf("parseOpenat2Resolve: parse value error expected %T received %T", r, resovle)
+		return fmt.Sprintf("%v", r), transformErr.Throwf("parseOpenat2Resolve: parse value error expected %T received %T", r, resovle)
 	}
 
 	var rs []string
@@ -339,190 +339,97 @@ func parseOpenat2Resolve(resovle any) (string, error) {
 }
 
 const (
-	AF_UNSPEC     = 0
-	AF_UNIX       = 1
-	AF_INET       = 2
-	AF_AX2        = 3
-	AF_IPX        = 4
-	AF_APPLETALK  = 5
-	AF_NETROM     = 6
-	AF_BRIDGE     = 7
-	AF_ATMPVC     = 8
-	AF_X25        = 9
-	AF_INET6      = 10
-	AF_ROSE       = 11
-	AF_DECnet     = 12
-	AF_NETBEUI    = 13
-	AF_SECURITY   = 14
-	AF_KEY        = 15
-	AF_NETLINK    = 16
-	AF_PACKET     = 17
-	AF_ASH        = 18
-	AF_ECONET     = 19
-	AF_ATMSVC     = 20
-	AF_RDS        = 21
-	AF_SNA        = 22
-	AF_IRDA       = 23
-	AF_PPPOX      = 24
-	AF_WANPIPE    = 25
-	AF_LLC        = 26
-	AF_IB         = 27
-	AF_MPLS       = 28
-	AF_CAN        = 29
-	AF_TIPC       = 30
-	AF_BLUETOOTH  = 31
-	AF_IUCV       = 32
-	AF_RXRPC      = 33
-	AF_ISDN       = 34
-	AF_PHONET     = 35
-	AF_IEEE802154 = 36
-	AF_CAIF       = 37
-	AF_ALG        = 38
-	AF_NFC        = 39
-	AF_VSOCK      = 40
-	AF_KCM        = 41
-	AF_QIPCRTR    = 42
-	AF_SMC        = 43
-	AF_XDP        = 44
+	AF_UNIX  = 1
+	AF_INET  = 2
+	AF_INET6 = 10
 )
+
+var socketFamilyNames = map[int32]string{
+	0:  "AF_UNSPEC",
+	1:  "AF_UNIX",
+	2:  "AF_INET",
+	3:  "AF_AX2",
+	4:  "AF_IPX",
+	5:  "AF_APPLETALK",
+	6:  "AF_NETROM",
+	7:  "AF_BRIDGE",
+	8:  "AF_ATMPVC",
+	9:  "AF_X25",
+	10: "AF_INET6",
+	11: "AF_ROSE",
+	12: "AF_DECnet",
+	13: "AF_NETBEUI",
+	14: "AF_SECURITY",
+	15: "AF_KEY",
+	16: "AF_NETLINK",
+	17: "AF_PACKET",
+	18: "AF_ASH",
+	19: "AF_ECONET",
+	20: "AF_ATMSVC",
+	21: "AF_RDS",
+	22: "AF_SNA",
+	23: "AF_IRDA",
+	24: "AF_PPPOX",
+	25: "AF_WANPIPE",
+	26: "AF_LLC",
+	27: "AF_IB",
+	28: "AF_MPLS",
+	29: "AF_CAN",
+	30: "AF_TIPC",
+	31: "AF_BLUETOOTH",
+	32: "AF_IUCV",
+	33: "AF_RXRPC",
+	34: "AF_ISDN",
+	35: "AF_PHONET",
+	36: "AF_IEEE802154",
+	37: "AF_CAIF",
+	38: "AF_ALG",
+	39: "AF_NFC",
+	40: "AF_VSOCK",
+	41: "AF_KCM",
+	42: "AF_QIPCRTR",
+	43: "AF_SMC",
+	44: "AF_XDP",
+}
 
 func parseSocketFamily(n any) (string, error) {
 	f, ok := n.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", f), fmt.Errorf("parseSocketFamily: parse value error")
+		return fmt.Sprintf("%v", f), transformErr.Throwf("parseSocketFamily: parse value error expected %T received %T", f, n)
 	}
 
-	switch f {
-	case AF_UNSPEC:
-		return "AF_UNSPEC", nil
-	case AF_UNIX:
-		return "AF_UNIX", nil
-	case AF_INET:
-		return "AF_INET", nil
-	case AF_AX2:
-		return "AF_AX2", nil
-	case AF_IPX:
-		return "AF_IPX", nil
-	case AF_APPLETALK:
-		return "AF_APPLETALK", nil
-	case AF_NETROM:
-		return "AF_NETROM", nil
-	case AF_BRIDGE:
-		return "AF_BRIDGE", nil
-	case AF_ATMPVC:
-		return "AF_ATMPVC", nil
-	case AF_X25:
-		return "AF_X25", nil
-	case AF_INET6:
-		return "AF_INET6", nil
-	case AF_ROSE:
-		return "AF_ROSE", nil
-	case AF_DECnet:
-		return "AF_DECnet", nil
-	case AF_NETBEUI:
-		return "AF_NETBEUI", nil
-	case AF_SECURITY:
-		return "AF_SECURITY", nil
-	case AF_KEY:
-		return "AF_KEY", nil
-	case AF_NETLINK:
-		return "AF_NETLINK", nil
-	case AF_PACKET:
-		return "AF_PACKET", nil
-	case AF_ASH:
-		return "AF_ASH", nil
-	case AF_ECONET:
-		return "AF_ECONET", nil
-	case AF_ATMSVC:
-		return "AF_ATMSVC", nil
-	case AF_RDS:
-		return "AF_RDS", nil
-	case AF_SNA:
-		return "AF_SNA", nil
-	case AF_IRDA:
-		return "AF_IRDA", nil
-	case AF_PPPOX:
-		return "AF_PPPOX", nil
-	case AF_WANPIPE:
-		return "AF_WANPIPE", nil
-	case AF_LLC:
-		return "AF_LLC", nil
-	case AF_IB:
-		return "AF_IB", nil
-	case AF_MPLS:
-		return "AF_MPLS", nil
-	case AF_CAN:
-		return "AF_CAN", nil
-	case AF_TIPC:
-		return "AF_TIPC", nil
-	case AF_BLUETOOTH:
-		return "AF_BLUETOOTH", nil
-	case AF_IUCV:
-		return "AF_IUCV", nil
-	case AF_RXRPC:
-		return "AF_RXRPC", nil
-	case AF_ISDN:
-		return "AF_ISDN", nil
-	case AF_PHONET:
-		return "AF_PHONET", nil
-	case AF_IEEE802154:
-		return "AF_IEEE802154", nil
-	case AF_CAIF:
-		return "AF_CAIF", nil
-	case AF_ALG:
-		return "AF_ALG", nil
-	case AF_NFC:
-		return "AF_NFC", nil
-	case AF_VSOCK:
-		return "AF_VSOCK", nil
-	case AF_KCM:
-		return "AF_KCM", nil
-	case AF_QIPCRTR:
-		return "AF_QIPCRTR", nil
-	case AF_SMC:
-		return "AF_SMC", nil
-	case AF_XDP:
-		return "AF_XDP", nil
-	default:
-		return fmt.Sprintf("%v", f), nil
+	if name, ok := socketFamilyNames[f]; ok {
+		return name, nil
 	}
+
+	return fmt.Sprintf("%v", f), nil
+}
+
+var socketTypes = map[int32]string{
+	1:  "SOCK_STREAM",
+	2:  "SOCK_DGRAM",
+	3:  "SOCK_RAW",
+	4:  "SOCK_RDM",
+	5:  "SOCK_SEQPACKET",
+	6:  "SOCK_DCCP",
+	10: "SOCK_PACKET",
 }
 
 const (
-	SOCK_STREAM    = 1
-	SOCK_DGRAM     = 2
-	SOCK_RAW       = 3
-	SOCK_RDM       = 4
-	SOCK_SEQPACKET = 5
-	SOCK_DCCP      = 6
-	SOCK_PACKET    = 10
-	SOCK_CLOEXEC   = 02000000
-	SOCK_NONBLOCK  = 00004000
+	SOCK_CLOEXEC  = 02000000
+	SOCK_NONBLOCK = 00004000
 )
 
 func parseSocketType(n any) (string, error) {
 	t, ok := n.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", t), fmt.Errorf("parseSocketType: parse value error")
+		return fmt.Sprintf("%v", t), transformErr.Throwf("parseSocketType: parse value error expected %T received %T", t, n)
 	}
 
 	var ts []string
-	switch t & 0xf {
-	case SOCK_STREAM:
-		ts = append(ts, "SOCK_STREAM")
-	case SOCK_DGRAM:
-		ts = append(ts, "SOCK_DGRAM")
-	case SOCK_RAW:
-		ts = append(ts, "SOCK_RAW")
-	case SOCK_RDM:
-		ts = append(ts, "SOCK_RDM")
-	case SOCK_SEQPACKET:
-		ts = append(ts, "SOCK_SEQPACKET")
-	case SOCK_DCCP:
-		ts = append(ts, "SOCK_DCCP")
-	case SOCK_PACKET:
-		ts = append(ts, "SOCK_PACKET")
-	default:
+	if tp, ok := socketTypes[t&0xf]; ok {
+		ts = append(ts, tp)
+	} else {
 		ts = append(ts, fmt.Sprintf("%v", t))
 	}
 
@@ -537,92 +444,43 @@ func parseSocketType(n any) (string, error) {
 	return strings.Join(ts, "|"), nil
 }
 
-const (
-	IPPROTO_IP      = 0
-	IPPROTO_ICMP    = 1
-	IPPROTO_IGMP    = 2
-	IPPROTO_IPIP    = 4
-	IPPROTO_TCP     = 6
-	IPPROTO_EGP     = 8
-	IPPROTO_PUP     = 12
-	IPPROTO_UDP     = 17
-	IPPROTO_IDP     = 22
-	IPPROTO_TP      = 29
-	IPPROTO_DCCP    = 33
-	IPPROTO_IPV6    = 41
-	IPPROTO_RSVP    = 46
-	IPPROTO_GRE     = 47
-	IPPROTO_ESP     = 50
-	IPPROTO_AH      = 51
-	IPPROTO_MTP     = 92
-	IPPROTO_BEETPH  = 94
-	IPPROTO_ENCAP   = 98
-	IPPROTO_PIM     = 103
-	IPPROTO_COMP    = 108
-	IPPROTO_SCTP    = 132
-	IPPROTO_UDPLITE = 136
-	IPPROTO_MPLS    = 137
-	IPPROTO_RAW     = 255
-)
+var socketProtocols = map[int32]string{
+	0:   "IPPROTO_IP",
+	1:   "IPPROTO_ICMP",
+	2:   "IPPROTO_IGMP",
+	4:   "IPPROTO_IPIP",
+	6:   "IPPROTO_TCP",
+	8:   "IPPROTO_EGP",
+	12:  "IPPROTO_PUP",
+	17:  "IPPROTO_UDP",
+	22:  "IPPROTO_IDP",
+	29:  "IPPROTO_TP",
+	33:  "IPPROTO_DCCP",
+	41:  "IPPROTO_IPV6",
+	46:  "IPPROTO_RSVP",
+	47:  "IPPROTO_GRE",
+	50:  "IPPROTO_ESP",
+	51:  "IPPROTO_AH",
+	92:  "IPPROTO_MTP",
+	94:  "IPPROTO_BEETPH",
+	98:  "IPPROTO_ENCAP",
+	103: "IPPROTO_PIM",
+	108: "IPPROTO_COMP",
+	132: "IPPROTO_SCTP",
+	136: "IPPROTO_UDPLITE",
+	137: "IPPROTO_MPLS",
+	255: "IPPROTO_RAW",
+}
 
 func parseSocketProtocol(n any) (string, error) {
 	p, ok := n.(int32)
 	if !ok {
-		return fmt.Sprintf("%v", p), fmt.Errorf("parseSocketProtocol: parse value error")
+		return fmt.Sprintf("%v", p), transformErr.Throwf("parseSocketProtocol: parse value error expected %T received %T", p, n)
 	}
 
-	switch p {
-	case IPPROTO_IP:
-		return "IPPROTO_IP", nil
-	case IPPROTO_ICMP:
-		return "IPPROTO_ICMP", nil
-	case IPPROTO_IGMP:
-		return "IPPROTO_IGMP", nil
-	case IPPROTO_IPIP:
-		return "IPPROTO_IPIP", nil
-	case IPPROTO_TCP:
-		return "IPPROTO_TCP", nil
-	case IPPROTO_EGP:
-		return "IPPROTO_EGP", nil
-	case IPPROTO_PUP:
-		return "IPPROTO_PUP", nil
-	case IPPROTO_UDP:
-		return "IPPROTO_UDP", nil
-	case IPPROTO_IDP:
-		return "IPPROTO_IDP", nil
-	case IPPROTO_TP:
-		return "IPPROTO_TP", nil
-	case IPPROTO_DCCP:
-		return "IPPROTO_DCCP", nil
-	case IPPROTO_IPV6:
-		return "IPPROTO_IPV6", nil
-	case IPPROTO_RSVP:
-		return "IPPROTO_RSVP", nil
-	case IPPROTO_GRE:
-		return "IPPROTO_GRE", nil
-	case IPPROTO_ESP:
-		return "IPPROTO_ESP", nil
-	case IPPROTO_AH:
-		return "IPPROTO_AH", nil
-	case IPPROTO_MTP:
-		return "IPPROTO_MTP", nil
-	case IPPROTO_BEETPH:
-		return "IPPROTO_BEETPH", nil
-	case IPPROTO_ENCAP:
-		return "IPPROTO_ENCAP", nil
-	case IPPROTO_PIM:
-		return "IPPROTO_PIM", nil
-	case IPPROTO_COMP:
-		return "IPPROTO_COMP", nil
-	case IPPROTO_SCTP:
-		return "IPPROTO_SCTP", nil
-	case IPPROTO_UDPLITE:
-		return "IPPROTO_UDPLITE", nil
-	case IPPROTO_MPLS:
-		return "IPPROTO_MPLS", nil
-	case IPPROTO_RAW:
-		return "IPPROTO_RAW", nil
-	default:
-		return fmt.Sprintf("%v", p), nil
+	if prot, ok := socketProtocols[p]; ok {
+		return prot, nil
 	}
+
+	return fmt.Sprintf("%v", p), nil
 }
