8a44280ef04f1215b5ca85f3f81541a955173635
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-16 13:04:14 -0600

    feat: add new package err

---
+++ pkg/err/err.go
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Authors of Tarian & the Organization created Tarian
+
+package err
+
+import "fmt"
+
+type Err struct {
+	Caller  string
+	Message string
+}
+
+func New(c string) *Err {
+	return &Err{
+		Caller:  c,
+		Message: "",
+	}
+}
+
+func (e *Err) Throwf(format string, a ...any) *Err {
+	e.Message = fmt.Sprintf(format, a...)
+
+	return e
+}
+
+func (e *Err) Throw(format string) *Err {
+	e.Message = fmt.Sprint(format)
+
+	return e
+}
+
+func (e *Err) Error() string {
+	return fmt.Sprintf("%sError: %s", e.Caller, e.Message)
+}
