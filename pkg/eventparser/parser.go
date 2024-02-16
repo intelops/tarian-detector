8edd2427909f021dbc78d0f27b57c528b5ee053e
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-16 13:03:49 -0600

    feat: add new package eventparser

---
+++ pkg/eventparser/parser.go
@@ -0,0 +1,303 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Authors of Tarian & the Organization created Tarian
+
+package eventparser
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/intelops/tarian-detector/pkg/err"
+	"github.com/intelops/tarian-detector/pkg/utils"
+)
+
+var parserErr = err.New("eventparser.parser")
+
+type ByteStream struct {
+	data     []byte
+	position int
+	nparams  uint8
+}
+
+func NewByteStream(inputData []byte, n uint8) *ByteStream {
+	return &ByteStream{
+		data:     inputData,
+		position: 0,
+		nparams:  n,
+	}
+}
+
+var Events TarianEventMap
+
+func ParseByteArray(data []byte) (map[string]any, error) {
+	/*
+		Assuming a specific byte pattern within the byte array:
+
+		tarianmetadata + params
+	*/
+	eventId, err := getEventId(data)
+	if err != nil {
+		return nil, parserErr.Throwf("%v", err)
+	}
+
+	event, noEvent := Events[eventId]
+	if !noEvent {
+		return nil, parserErr.Throwf("missing event from var Events TarianEventMap for key: %v", eventId)
+	}
+
+	var metaData TarianMetaData
+	lenMetaData := binary.Size(metaData)
+	err = binary.Read(bytes.NewReader(data[:lenMetaData]), binary.LittleEndian, &metaData)
+	if err != nil {
+		return nil, parserErr.Throwf("%v", err)
+	}
+
+	if metaData.MetaData.Syscall != int32(event.syscallId) {
+		metaData.MetaData.Syscall = int32(event.syscallId)
+	}
+
+	record := toMap(metaData)
+	record["eventId"] = event.name
+
+	bs := NewByteStream(data[lenMetaData:], metaData.MetaData.Nparams)
+	ps, err := bs.parseParams(event)
+	if err != nil {
+		return nil, parserErr.Throwf("%v", err)
+	}
+
+	record["context"] = ps
+
+	return record, nil
+}
+
+func (bs *ByteStream) parseParams(event TarianEvent) ([]arg, error) {
+	tParams := event.params
+	if len(tParams) <= 0 {
+		return nil, parserErr.Throwf("missing event from var Events TarianEventMap")
+	}
+
+	var args []arg
+
+	for i := 0; i < int(bs.nparams); i++ {
+		if bs.position >= len(bs.data) {
+			break
+		}
+
+		if i >= len(tParams) {
+			break
+		}
+
+		ag, err := bs.parseParam(tParams[i])
+		if err != nil {
+			return nil, parserErr.Throwf("%v", err)
+		}
+
+		args = append(args, ag)
+	}
+
+	return args, nil
+}
+
+func (bs *ByteStream) parseParam(p Param) (arg, error) {
+	var pVal any
+	var err error
+
+	switch p.paramType {
+	case TDT_U8:
+		pVal, err = utils.Uint8(bs.data[bs.position : bs.position+1])
+		bs.position += 1
+	case TDT_U16:
+		pVal, err = utils.Uint16(bs.data[bs.position : bs.position+2])
+		bs.position += 2
+	case TDT_U32:
+		pVal, err = utils.Uint32(bs.data[bs.position : bs.position+4])
+		bs.position += 4
+	case TDT_U64:
+		pVal, err = utils.Uint64(bs.data[bs.position : bs.position+8])
+		bs.position += 8
+	case TDT_S8:
+		pVal, err = utils.Int8(bs.data[bs.position : bs.position+1])
+		bs.position += 1
+	case TDT_S16:
+		pVal, err = utils.Int16(bs.data[bs.position : bs.position+2])
+		bs.position += 2
+	case TDT_S32:
+		pVal, err = utils.Int32(bs.data[bs.position : bs.position+4])
+		bs.position += 4
+	case TDT_S64:
+		pVal, err = utils.Int64(bs.data[bs.position : bs.position+8])
+		bs.position += 8
+	case TDT_STR, TDT_STR_ARR:
+		slen, err := bs.readShort()
+		if err != nil {
+			return arg{}, parserErr.Throwf("%v", err)
+		}
+
+		pVal = utils.ToString(bs.data[bs.position : bs.position+int(slen)])
+		bs.position += int(slen)
+	case TDT_BYTE_ARR:
+		slen, err := bs.readShort()
+		if err != nil {
+			return arg{}, parserErr.Throwf("%v", err)
+		}
+
+		pVal = fmt.Sprintf("%v", bs.data[bs.position:bs.position+int(slen)])
+		bs.position += int(slen)
+
+	case TDT_SOCKADDR:
+		family, err := bs.readByte()
+		if err != nil {
+			return arg{}, parserErr.Throwf("%v", err)
+		}
+
+		switch family {
+		case AF_INET:
+			{
+				type sockaddr_in struct {
+					Family  string
+					Sa_addr string
+					Sa_port uint16
+				}
+
+				var addr sockaddr_in
+				addr.Family = "AF_INET"
+
+				ipv4_arr := bs.data[bs.position : bs.position+4]
+				bs.position += 4
+
+				addr.Sa_addr = utils.Ipv4([4]byte(ipv4_arr))
+
+				port, err := bs.readShort()
+				if err != nil {
+					return arg{}, parserErr.Throwf("%v", err)
+				}
+
+				addr.Sa_port = utils.Ntohs(port)
+
+				pVal = fmt.Sprintf("%+v", addr)
+			}
+		case AF_INET6:
+			{
+				type sockaddr_in6 struct {
+					Family  string
+					Sa_addr string
+					Sa_port uint16
+				}
+
+				var addr sockaddr_in6
+				addr.Family = "AF_INET6"
+
+				ipv6_arr := bs.data[bs.position : bs.position+16]
+				bs.position += 16
+
+				addr.Sa_addr = utils.Ipv6([16]byte(ipv6_arr))
+
+				port, err := bs.readShort()
+				if err != nil {
+					return arg{}, parserErr.Throwf("%v", err)
+				}
+
+				addr.Sa_port = utils.Ntohs(port)
+
+				pVal = fmt.Sprintf("%+v", addr)
+			}
+		case AF_UNIX:
+			{
+				type sockaddr_un struct {
+					Family   string
+					Sun_path string
+				}
+
+				var addr sockaddr_un
+				addr.Family = "AF_UNIX"
+
+				slen, err := bs.readShort()
+				if err != nil {
+					return arg{}, parserErr.Throwf("%v", err)
+				}
+
+				addr.Sun_path = utils.ToString(bs.data[bs.position : bs.position+int(slen)])
+				bs.position += int(slen)
+
+				pVal = fmt.Sprintf("%+v", addr)
+			}
+		}
+	}
+
+	if err != nil {
+		return arg{}, parserErr.Throwf("%v", err)
+	}
+
+	return p.processValue(pVal)
+}
+
+func (bs *ByteStream) readByte() (uint8, error) {
+	bt, err := utils.Uint8(bs.data[bs.position : bs.position+1])
+	if err != nil {
+		return 0, parserErr.Throwf("%v", err)
+	}
+
+	bs.position += 1
+	return bt, nil
+}
+
+func (bs *ByteStream) readShort() (uint16, error) {
+	sh, err := utils.Uint16(bs.data[bs.position : bs.position+2])
+	if err != nil {
+		return 0, parserErr.Throwf("%v", err)
+	}
+
+	bs.position += 2
+	return sh, nil
+}
+
+func getEventId(data []byte) (int, error) {
+	if len(data) < 4 {
+		return 0, parserErr.Throwf("input data length is %d, expected at least %d", len(data), 4)
+	}
+
+	var id int32
+	err := binary.Read(bytes.NewReader(data[:4]), binary.LittleEndian, &id)
+	if err != nil {
+		return 0, parserErr.Throwf("failed to read event ID from data: %v", err)
+	}
+
+	return int(id), nil
+}
+
+func toMap(t TarianMetaData) map[string]any {
+	m := make(map[string]any)
+
+	m["eventName"] = t.MetaData.Event
+	m["timestamp"] = t.MetaData.Ts
+	m["syscallId"] = t.MetaData.Syscall
+	m["processor"] = t.MetaData.Processor
+
+	// task
+	m["threadStartTime"] = t.MetaData.Task.StartTime
+	m["hostProcessId"] = t.MetaData.Task.HostPid
+	m["hostThreadId"] = t.MetaData.Task.HostTgid
+	m["hostParentProcessId"] = t.MetaData.Task.HostPpid
+	m["processId"] = t.MetaData.Task.Pid
+	m["threadId"] = t.MetaData.Task.Tgid
+	m["parentProcessId"] = t.MetaData.Task.Ppid
+	m["userId"] = t.MetaData.Task.Uid
+	m["groupId"] = t.MetaData.Task.Gid
+	m["cgroupId"] = t.MetaData.Task.CgroupId
+	m["mountNamespace"] = t.MetaData.Task.MountNsId
+	m["pidNamespace"] = t.MetaData.Task.PidNsId
+	m["execId"] = t.MetaData.Task.ExecId
+	m["parentExecId"] = t.MetaData.Task.ParentExecId
+	m["processName"] = utils.ToString(t.MetaData.Task.Comm[:])
+	m["directory"] = utils.ToString(t.MetaData.Task.Cwd[:])
+
+	m["sysname"] = utils.ToString(t.SystemInfo.Sysname[:])
+	m["nodename"] = utils.ToString(t.SystemInfo.Nodename[:])
+	m["release"] = utils.ToString(t.SystemInfo.Release[:])
+	m["version"] = utils.ToString(t.SystemInfo.Version[:])
+	m["machine"] = utils.ToString(t.SystemInfo.Machine[:])
+	m["domainname"] = utils.ToString(t.SystemInfo.Domainname[:])
+
+	return m
+}
---
+++ pkg/eventparser/probes.go
@@ -0,0 +1,275 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Authors of Tarian & the Organization created Tarian
+
+package eventparser
+
+import (
+	"fmt"
+
+	"github.com/intelops/tarian-detector/pkg/err"
+)
+
+var probesErr = err.New("eventparser.probes")
+
+type arg struct {
+	Name       string
+	Value      string
+	TarianType uint32
+	LinuxType  string
+}
+
+type Param struct {
+	name      string
+	paramType TarianParamType
+	linuxType string
+	function  func(any) (string, error)
+}
+
+type TarianEvent struct {
+	name      string
+	syscallId int
+	eventSize uint32
+	params    []Param
+}
+
+type TarianEventMap map[int]TarianEvent
+
+func (te TarianEventMap) AddTarianEvent(idx int, event TarianEvent) {
+	te[idx] = event
+}
+
+func NewTarianEvent(id int, name string, size uint32, params ...Param) TarianEvent {
+	return TarianEvent{
+		name:      name,
+		syscallId: id,
+		eventSize: size,
+		params:    params,
+	}
+}
+
+func LoadTarianEvents() {
+	Events = GenerateTarianEvents()
+}
+
+func GenerateTarianEvents() TarianEventMap {
+	events := make(TarianEventMap)
+
+	execve_e := NewTarianEvent(59, "sys_execve_entry", 8957,
+		Param{name: "filename", paramType: TDT_STR, linuxType: "const char *"},
+		Param{name: "argv", paramType: TDT_STR_ARR, linuxType: "const char **"},
+		Param{name: "envp", paramType: TDT_STR_ARR, linuxType: "const char **"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_EXECVE_E, execve_e)
+
+	execve_r := NewTarianEvent(59, "sys_execve_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_EXECVE_R, execve_r)
+
+	execveat_e := NewTarianEvent(322, "sys_execveat_entry", 8965,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int", function: parseExecveatDird},
+		Param{name: "filename", paramType: TDT_STR, linuxType: "const char *"},
+		Param{name: "argv", paramType: TDT_STR_ARR, linuxType: "char const **"},
+		Param{name: "envp", paramType: TDT_STR_ARR, linuxType: "char const **"},
+		Param{name: "flags", paramType: TDT_S32, linuxType: "int", function: parseExecveatFlags},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_EXECVEAT_E, execveat_e)
+
+	execveat_r := NewTarianEvent(322, "sys_execveat_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_EXECVEAT_R, execveat_r)
+
+	clone_e := NewTarianEvent(56, "sys_clone_entry", 793,
+		Param{name: "clone_flags", paramType: TDT_U64, linuxType: "unsigned long", function: parseCloneFlags},
+		Param{name: "newsp", paramType: TDT_S64, linuxType: "unsigned long"},
+		Param{name: "parent_tid", paramType: TDT_S32, linuxType: "int *"},
+		Param{name: "child_tid", paramType: TDT_S32, linuxType: "int *"},
+		Param{name: "tls", paramType: TDT_S64, linuxType: "unsigned long"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CLONE_E, clone_e)
+
+	clone_r := NewTarianEvent(56, "sys_clone_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CLONE_R, clone_r)
+
+	close_e := NewTarianEvent(3, "sys_close_entry", 765,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CLOSE_E, close_e)
+
+	close_r := NewTarianEvent(3, "sys_close_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CLOSE_R, close_r)
+
+	read_e := NewTarianEvent(0, "sys_read_entry", 4867,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "buf", paramType: TDT_BYTE_ARR, linuxType: "char *"},
+		Param{name: "count", paramType: TDT_U32, linuxType: "size_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_READ_E, read_e)
+
+	read_r := NewTarianEvent(0, "sys_read_exit", 769,
+		Param{name: "return", paramType: TDT_S64, linuxType: "ssize_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_READ_R, read_r)
+
+	write_e := NewTarianEvent(1, "sys_write_entry", 4867,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "buf", paramType: TDT_BYTE_ARR, linuxType: "const char *"},
+		Param{name: "count", paramType: TDT_U32, linuxType: "size_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_WRITE_E, write_e)
+
+	write_r := NewTarianEvent(1, "sys_write_exit", 769,
+		Param{name: "return", paramType: TDT_S64, linuxType: "ssize_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_WRITE_R, write_r)
+
+	open_e := NewTarianEvent(2, "sys_open_entry", 4867,
+		Param{name: "filename", paramType: TDT_STR, linuxType: "const char *"},
+		Param{name: "flags", paramType: TDT_S32, linuxType: "int", function: parseOpenFlags},
+		Param{name: "mode", paramType: TDT_U32, linuxType: "umode_t", function: parseOpenMode},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPEN_E, open_e)
+
+	open_r := NewTarianEvent(2, "sys_open_exit", 765,
+		Param{name: "return", paramType: TDT_U32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPEN_R, open_r)
+
+	readv_e := NewTarianEvent(19, "sys_readv_entry", 4867,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "vec", paramType: TDT_BYTE_ARR, linuxType: "const struct iovec *"},
+		Param{name: "vlen", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_READV_E, readv_e)
+
+	readv_r := NewTarianEvent(19, "sys_readv_exit", 769,
+		Param{name: "return", paramType: TDT_S64, linuxType: "ssize_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_READV_R, readv_r)
+
+	writev_e := NewTarianEvent(20, "sys_writev_entry", 4867,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "vec", paramType: TDT_BYTE_ARR, linuxType: "const struct iovec *"},
+		Param{name: "vlen", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_WRITEV_E, writev_e)
+
+	writev_r := NewTarianEvent(20, "sys_writev_exit", 769,
+		Param{name: "return", paramType: TDT_S64, linuxType: "ssize_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_WRITEV_R, writev_r)
+
+	openat_e := NewTarianEvent(257, "sys_openat_entry", 4871,
+		Param{name: "dfd", paramType: TDT_S32, linuxType: "int", function: parseExecveatDird},
+		Param{name: "filename", paramType: TDT_STR, linuxType: "const char *"},
+		Param{name: "flags", paramType: TDT_S32, linuxType: "int", function: parseOpenFlags},
+		Param{name: "mode", paramType: TDT_U32, linuxType: "umode_t", function: parseOpenMode},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPENAT_E, openat_e)
+
+	openat_r := NewTarianEvent(257, "sys_openat_exit", 765,
+		Param{name: "return", paramType: TDT_U32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPENAT_R, openat_r)
+
+	openat2_e := NewTarianEvent(437, "sys_openat2_entry", 4891,
+		Param{name: "dfd", paramType: TDT_S32, linuxType: "int", function: parseExecveatDird},
+		Param{name: "filename", paramType: TDT_STR, linuxType: "const char *"},
+		Param{name: "flags", paramType: TDT_S64, linuxType: "unsigned long", function: parseOpenat2Flags},
+		Param{name: "mode", paramType: TDT_S64, linuxType: "unsigned long", function: parseOpenat2Mode},
+		Param{name: "resolve", paramType: TDT_S64, linuxType: "unsigned long", function: parseOpenat2Resolve},
+		Param{name: "usize", paramType: TDT_S32, linuxType: "size_t"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPENAT2_E, openat2_e)
+
+	openat2_r := NewTarianEvent(437, "sys_openat2_exit", 769,
+		Param{name: "return", paramType: TDT_S64, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_OPENAT2_R, openat2_r)
+
+	listen_e := NewTarianEvent(50, "sys_listen_entry", 769,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "backlog", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_LISTEN_E, listen_e)
+
+	listen_r := NewTarianEvent(50, "sys_listen_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_LISTEN_R, listen_r)
+
+	socket_e := NewTarianEvent(41, "sys_socket_entry", 773,
+		Param{name: "family", paramType: TDT_S32, linuxType: "int", function: parseSocketFamily},
+		Param{name: "type", paramType: TDT_S32, linuxType: "int", function: parseSocketType},
+		Param{name: "protocol", paramType: TDT_S32, linuxType: "int", function: parseSocketProtocol},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_SOCKET_E, socket_e)
+
+	socket_r := NewTarianEvent(41, "sys_socket_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_SOCKET_R, socket_r)
+
+	accept_e := NewTarianEvent(43, "sys_accept_entry", 880,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "upeer_sockaddr", paramType: TDT_SOCKADDR, linuxType: "struct sockaddr *"},
+		Param{name: "upper_addrlen", paramType: TDT_S32, linuxType: "int *"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_ACCEPT_E, accept_e)
+
+	accept_r := NewTarianEvent(43, "sys_accept_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_ACCEPT_R, accept_r)
+
+	bind_e := NewTarianEvent(49, "sys_bind_entry", 880,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "umyaddr", paramType: TDT_SOCKADDR, linuxType: "struct sockaddr *"},
+		Param{name: "addrlen", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_BIND_E, bind_e)
+
+	bind_r := NewTarianEvent(49, "sys_bind_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_BIND_R, bind_r)
+
+	connect_e := NewTarianEvent(42, "sys_connect_entry", 880,
+		Param{name: "fd", paramType: TDT_S32, linuxType: "int"},
+		Param{name: "uservaddr", paramType: TDT_SOCKADDR, linuxType: "struct sockaddr *"},
+		Param{name: "addrlen", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CONNECT_E, connect_e)
+
+	connect_r := NewTarianEvent(42, "sys_connect_exit", 765,
+		Param{name: "return", paramType: TDT_S32, linuxType: "int"},
+	)
+	events.AddTarianEvent(TDE_SYSCALL_CONNECT_R, connect_r)
+
+	return events
+}
+
+func (p *Param) processValue(val interface{}) (arg, error) {
+	arg := arg{}
+
+	if p.function != nil {
+		parsedValue, err := p.function(val)
+		if err != nil {
+			return arg, probesErr.Throwf("%v", err)
+		}
+		arg.Value = parsedValue
+	} else {
+		arg.Value = fmt.Sprintf("%v", val)
+	}
+
+	arg.Name = p.name
+	arg.LinuxType = p.linuxType
+	arg.TarianType = uint32(p.paramType)
+
+	return arg, nil
+}
