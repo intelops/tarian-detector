7583c833b876a26be45b4c05fac1da8738ba9305
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-18 20:42:37 -0600

    refactor:  code optimized

--- pkg/utils/converter.go
+++ pkg/utils/converter.go
@@ -4,8 +4,8 @@
 package utils
 
 import (
-	"bytes"
 	"encoding/binary"
+	"net"
 	"strings"
 
 	"github.com/intelops/tarian-detector/pkg/err"
@@ -13,86 +13,104 @@ import (
 
 var converterErr = err.New("utils.converter")
 
-func Int8(b []byte) (int8, error) {
-	var num int8
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+const (
+	ErrOutOfBound string = "position %d out of range for data of length %d"
+)
+
+func Int8(b []byte, pos int) (int8, error) {
+	if pos < 0 || pos >= len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return int8(b[pos]), nil
 }
 
-func Int16(b []byte) (int16, error) {
-	var num int16
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Int16(b []byte, pos int) (int16, error) {
+	if pos < 0 || pos+2 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return int16(binary.LittleEndian.Uint16(b[pos : pos+2])), nil
 }
 
-func Int32(b []byte) (int32, error) {
-	var num int32
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Int32(b []byte, pos int) (int32, error) {
+	if pos < 0 || pos+2 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return int32(binary.LittleEndian.Uint32(b[pos : pos+4])), nil
 }
 
-func Int64(b []byte) (int64, error) {
-	var num int64
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Int64(b []byte, pos int) (int64, error) {
+	if pos < 0 || pos+2 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return int64(binary.LittleEndian.Uint64(b[pos : pos+8])), nil
 }
 
-func Uint8(b []byte) (uint8, error) {
-	var num uint8
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Uint8(b []byte, pos int) (uint8, error) {
+	if pos < 0 || pos >= len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return uint8(b[pos]), nil
 }
 
-func Uint16(b []byte) (uint16, error) {
-	var num uint16
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Uint16(b []byte, pos int) (uint16, error) {
+	if pos < 0 || pos+2 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return binary.LittleEndian.Uint16(b[pos : pos+2]), nil
 }
 
-func Uint32(b []byte) (uint32, error) {
-	var num uint32
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Uint32(b []byte, pos int) (uint32, error) {
+	if pos < 0 || pos+4 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return binary.LittleEndian.Uint32(b[pos : pos+4]), nil
 }
 
-func Uint64(b []byte) (uint64, error) {
-	var num uint64
-	err := binary.Read(bytes.NewReader(b), binary.LittleEndian, &num)
-	if err != nil {
-		return num, converterErr.Throwf("%v", err)
+func Uint64(b []byte, pos int) (uint64, error) {
+	if pos < 0 || pos+8 > len(b) {
+		return 0, converterErr.Throwf(ErrOutOfBound, pos, len(b))
 	}
 
-	return num, nil
+	return binary.LittleEndian.Uint64(b[pos : pos+8]), nil
 }
 
-func ToString(arr []byte) string {
-	return strings.Trim(string(arr), "\x00")
+func ToString(b []byte, pos int, size int) string {
+	if pos < 0 || pos+size > len(b) {
+		return ""
+	}
+
+	return strings.Trim(string(b[pos:pos+size]), "\x00")
+}
+
+// Ipv4 converts a byte array to an IPv4 string
+func Ipv4(b []byte, pos int) string {
+	if pos < 0 || pos+4 > len(b) {
+		return ""
+	}
+
+	return net.IP(b[pos : pos+4]).String()
+}
+
+// Ipv6 converts byte array to IPv6 string
+func Ipv6(b []byte, pos int) string {
+	if pos < 0 || pos+16 > len(b) {
+		return ""
+	}
+
+	return net.IP(b[pos : pos+16]).String()
+}
+
+// Ntohs converts little-endian uint16 to big-endian uint16
+func Ntohs(n uint16) uint16 {
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, n)
+
+	return binary.BigEndian.Uint16(b)
 }
