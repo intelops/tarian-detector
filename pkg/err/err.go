07df4429bbe981821d848847e0ac49f834f313a8
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-20 13:38:23 -0600

    doc: update comments and doc strings

--- pkg/err/err.go
+++ pkg/err/err.go
@@ -1,34 +1,43 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2024 Authors of Tarian & the Organization created Tarian
 
+// Package err provides error handling functionalities.
 package err
 
 import "fmt"
 
+// Err is an exported type that encapsulates an error with a caller and a message.
 type Err struct {
-	Caller  string
-	Message string
+	caller  string
+	message string
 }
 
-func New(c string) *Err {
+// New create new instance of Err with specified caller and an empty message.
+// It returns the pointer to the created Err.
+func New(caller string) *Err {
 	return &Err{
-		Caller:  c,
-		Message: "",
+		caller:  caller,
+		message: "",
 	}
 }
 
+// Throwf formats an error message according to a format specifier and returns the error.
+// It updates the message of the Err with the formatted message.
 func (e *Err) Throwf(format string, a ...any) *Err {
-	e.Message = fmt.Sprintf(format, a...)
+	e.message = fmt.Sprintf(format, a...)
 
 	return e
 }
 
-func (e *Err) Throw(format string) *Err {
-	e.Message = fmt.Sprint(format)
+// Throw updates the error message with the passed message and returns the error.
+func (e *Err) Throw(message string) *Err {
+	e.message = fmt.Sprint(message)
 
 	return e
 }
 
+// Error returns a string that represents the Err.
+// It combines the caller and the message of the Err.
 func (e *Err) Error() string {
-	return fmt.Sprintf("%sError: %s", e.Caller, e.Message)
+	return fmt.Sprintf("%sError: %s", e.caller, e.message)
 }
