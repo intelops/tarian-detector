a311820ee5361627170a46e070ba31e21af52b71
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-02-21 11:05:33 -0600

    feat: update unit test cases

--- pkg/err/err_test.go
+++ pkg/err/err_test.go
@@ -3,161 +3,232 @@
 
 package err
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 // TestNew tests the New function. It checks if the New function returns an error with the correct caller and an empty message.
 func TestNew(t *testing.T) {
-	// testCases contains the test cases for the TestNew function.
-	testCases := []struct {
-		name   string
+	type args struct {
 		caller string
-	}{
-		{
-			name:   "Empty Caller",
-			caller: "",
-		}, {
-			name:   "Non Empty Caller",
-			caller: "test",
-		},
 	}
-
-	// Iterate over the test cases.
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
-			// Call the New function and check the returned error.
-			err := New(test.caller)
-
-			// Check if the caller of the error is correct.
-			if err.caller != test.caller {
-				t.Errorf("Expected caller to be %s, got %s", test.caller, err.caller)
-			}
-
-			// Check if the message of the error is empty.
-			if err.message != "" {
-				t.Errorf("Expected message to be empty, got %q", err.message)
-			}
-		})
-	}
-}
-
-// TestThrowf tests the Throwf method. It checks if the Throwf method returns an error with the correct message.
-func TestThrowf(t *testing.T) {
-	// testCases contains the test cases for the TestThrowf function.
-	testCases := []struct {
-		name            string
-		format          string
-		args            []any
-		expectedMessage string
-	}{
-		{
-			name:            "No Argument",
-			format:          "This is an simple error message.",
-			args:            []any{},
-			expectedMessage: "This is an simple error message.",
-		}, {
-			name:            "Single Argument",
-			format:          "This is a formatted error message: %d",
-			args:            []any{42},
-			expectedMessage: "This is a formatted error message: 42",
-		}, {
-			name:            "Multiple Arguments",
-			format:          "Formatted message with %s and %d",
-			args:            []any{"string", 42},
-			expectedMessage: "Formatted message with string and 42",
-		}, {
-			name:            "Invalid Arguments",
-			format:          "This is an error proned format string %d %s",
-			args:            []any{10, true},
-			expectedMessage: "This is an error proned format string 10 %!s(bool=true)",
-		}, {
-			name:            "Missing Arguments",
-			format:          "This is an error proned format string %d %s",
-			args:            []any{},
-			expectedMessage: "This is an error proned format string %!d(MISSING) %!s(MISSING)",
-		},
-	}
-
-	// Iterate over the test cases.
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
-			// Create a new error and call the Throwf method.
-			caller := "TestThrowf"
-			err := New(caller).Throwf(test.format, test.args...)
-
-			// Check if the message of the error is correct.
-			if err.message != test.expectedMessage {
-				t.Errorf("Expected message to be %s, got %s", test.expectedMessage, err.message)
-			}
-		})
-	}
-}
-
-// TestThrow tests the Throw method. It checks if the Throw method returns an error with the correct message.
-func TestThrow(t *testing.T) {
-	// tests contains the test cases for the TestThrow function.
 	tests := []struct {
-		name            string
-		message         string
-		expectedMessage string
+		name string
+		args args
+		want *Err
 	}{
 		{
-			name:            "Simple Message",
-			message:         "This is an error message",
-			expectedMessage: "This is an error message",
+			name: "valid values",
+			args: args{
+				caller: "TestNew",
+			},
+			want: &Err{
+				caller: "TestNew",
+			},
+		}, {
+			name: "empty caller",
+			args: args{
+				caller: "",
+			},
+			want: &Err{
+				caller: "",
+			},
+		}, {
+			name: "check for empty message",
+			args: args{
+				caller: "TestNew",
+			},
+			want: &Err{
+				caller:  "TestNew",
+				message: "",
+			},
 		},
 	}
 
-	// Iterate over the test cases.
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Create a new error and call the Throw method.
-			caller := "TestThrow"
-			err := New(caller).Throw(tt.message)
-
-			// Check if the message of the error is correct.
-			if err.message != tt.expectedMessage {
-				t.Errorf("Expected message to be %s, got %s", tt.expectedMessage, err.message)
+			if got := New(tt.args.caller); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-// TestError tests the Error method. It checks if the Error method returns the correct error message.
-func TestError(t *testing.T) {
-	// testCases contains the test cases for the TestError function.
-	testCases := []struct {
-		name          string
-		caller        string
-		callFunc      func(e *Err) *Err
-		expectedError string
+// TestErr_Throw tests the Throw method. It checks if the Throw method returns an error with the correct message.
+func TestErr_Throwf(t *testing.T) {
+	type fields struct {
+		caller  string
+		message string
+	}
+	type args struct {
+		format string
+		a      []any
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Err
 	}{
 		{
-			name:   "Throw",
-			caller: "TestThrow",
-			callFunc: func(e *Err) *Err {
-				return e.Throw("Error message")
+			name: "valid values",
+			fields: fields{
+				caller:  "TestErr_Throwf",
+				message: "This is an error message",
 			},
-			expectedError: "TestThrowError: Error message",
+			args: args{
+				format: "This is a formatted error message: %d",
+				a:      []any{42},
+			},
+			want: &Err{
+				caller:  "TestErr_Throwf",
+				message: "This is a formatted error message: 42",
+			},
 		}, {
-			name:   "Throwf",
-			caller: "TestThrowf",
-			callFunc: func(e *Err) *Err {
-				return e.Throwf("Error number %d", 80)
+			name: "empty values",
+			fields: fields{
+				caller:  "TestErr_Throwf",
+				message: "This is an error message",
 			},
-			expectedError: "TestThrowfError: Error number 80",
+			args: args{
+				format: "This is a formatted error message: %d",
+				a:      []any{},
+			},
+			want: &Err{
+				caller:  "TestErr_Throwf",
+				message: "This is a formatted error message: %!d(MISSING)",
+			},
+		}, {
+			name: "single argument",
+			fields: fields{
+				caller:  "TestErr_Throwf",
+				message: "This is an error message",
+			},
+			args: args{
+				format: "This is a formatted error message: %d",
+				a:      []any{42},
+			},
+			want: &Err{
+				caller:  "TestErr_Throwf",
+				message: "This is a formatted error message: 42",
+			},
+		}, {
+			name: "multiple arguments",
+			fields: fields{
+				caller: "TestErr_Throwf",
+			},
+			args: args{
+				format: "This is a formatted error message: %d %s",
+				a:      []any{42, "error"},
+			},
+			want: &Err{
+				caller:  "TestErr_Throwf",
+				message: "This is a formatted error message: 42 error",
+			},
 		},
 	}
 
-	// Iterate over the test cases.
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
-			// Create a new error and call the test function
-			err := New(test.caller)
-			e := test.callFunc(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &Err{
+				caller:  tt.fields.caller,
+				message: tt.fields.message,
+			}
+			if got := e.Throwf(tt.args.format, tt.args.a...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Err.Throwf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-			// Call the Error method and check the returned error message.
-			if errorMessage := e.Error(); errorMessage != test.expectedError {
-				t.Errorf("Expected error message to be %s, got %s", test.expectedError, errorMessage)
+// TestErr_Throw tests the Throw method. It checks if the Throw method returns an error with the correct message.
+func TestErr_Throw(t *testing.T) {
+	type fields struct {
+		caller  string
+		message string
+	}
+	type args struct {
+		message string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Err
+	}{
+		{
+			name: "valid values",
+			fields: fields{
+				caller:  "TestErr_Throw",
+				message: "This is an error message",
+			},
+			args: args{
+				message: "This is an error message",
+			},
+			want: &Err{
+				caller:  "TestErr_Throw",
+				message: "This is an error message",
+			},
+		}, {
+			name: "empty values",
+			fields: fields{
+				caller:  "TestErr_Throw",
+				message: "This is an error message",
+			},
+			args: args{
+				message: "",
+			},
+			want: &Err{
+				caller:  "TestErr_Throw",
+				message: "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &Err{
+				caller:  tt.fields.caller,
+				message: tt.fields.message,
+			}
+			if got := e.Throw(tt.args.message); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Err.Throw() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// TestErr_Error tests the Error method. It checks if the Error method returns the correct error message.
+func TestErr_Error(t *testing.T) {
+	type fields struct {
+		caller  string
+		message string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid values",
+			fields: fields{
+				caller:  "TestErr_Error",
+				message: "This is an error message",
+			},
+			want: "TestErr_ErrorError: This is an error message",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &Err{
+				caller:  tt.fields.caller,
+				message: tt.fields.message,
+			}
+
+			if got := e.Error(); got != tt.want {
+				t.Errorf("Err.Error() = %v, want %v", got, tt.want)
 			}
 		})
 	}
