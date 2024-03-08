ba8620833d01729dd1aafb22b7e918ccc7ebe892
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-03-08 15:34:08 -0600

    feat: add docstrings and comments

---
+++ pkg/k8s/doc.go
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Authors of Tarian & the Organization created Tarian
+
+// Package k8s provides tools for managing Pods and Containers in a Kubernetes environment.
+// It includes functionalities for watching Pods, indexing them by container IDs, and finding a Pod given a container ID.
+package k8s
