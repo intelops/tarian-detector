da7e92c5bccd2cf342873a0afe7226d697b206eb
Charan Ravela <124630918+c-ravela@users.noreply.github.com>
2024-03-08 14:56:01 -0600

    feat: update docstrings and comments

---
+++ pkg/eventparser/doc.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Authors of Tarian & the Organization created Tarian
+
+// Package eventparser provides functions to parse raw byte array data into a more readable format.
+//
+// It handles the functionality for parsing events, generating static events information,  and provides utility functions for data transformation.
+package eventparser
